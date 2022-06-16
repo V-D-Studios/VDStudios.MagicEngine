cc8cbd6d28fd97a0817810788de5920e850d02c6
Diego García <dagarciam1014@gmail.com>
2022-06-16 01:51:14 -0400

    Create GameObject.cs

---
+++ VDStudios.MagicEngine/GameObject.cs
@@ -0,0 +1,23 @@
+﻿namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// Represents an object of the <see cref="Game"/>
+/// </summary>
+/// <remarks>
+/// This class cannot be inherited directly by user code
+/// </remarks>
+public class GameObject
+{
+    /// <summary>
+    /// Instances a new GameObject
+    /// </summary>
+    internal GameObject()
+    {
+        Game = Game.Instance;
+    }
+
+    /// <summary>
+    /// The <see cref="MagicEngine.Game"/> this <see cref="Scene"/> belongs to. Same as<see cref="SDL2.NET.SDLApplication{TApp}.Instance"/>
+    /// </summary>
+    protected Game Game { get; }
+}
