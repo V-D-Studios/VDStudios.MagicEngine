5aa39120291395061e3e7a62d3f40b979ba8cb3a
Diego García <dagarciam1014@gmail.com>
2022-06-16 17:34:17 -0400

    Began work on DrawQueue
    Added necesary fields and defined necessary classes, as well as implemented interfaces

---
+++ VDStudios.MagicEngine/Internal/DrawQueue.cs
@@ -0,0 +1,25 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine.Internal;
+
+internal sealed class DrawQueue : IDrawQueue
+{
+    #region Fields
+
+    private readonly PriorityQueue<IDrawing, float> _queue = new(20, new PriorityComparer());
+    private readonly AsyncLock _lock = new();
+
+    #endregion
+    #region Comparer
+
+    private class PriorityComparer : IComparer<float>
+    {
+        public int Compare(float x, float y) => x > y ? 1 : -1;
+    }
+    #endregion
+}
