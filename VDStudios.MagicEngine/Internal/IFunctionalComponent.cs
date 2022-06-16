fcb665d444e3f2cff9508d607b57f5ccddc2ed50
Diego García <dagarciam1014@gmail.com>
2022-06-16 02:05:21 -0400

    Create IFunctionalComponent.cs

---
+++ VDStudios.MagicEngine/Internal/IFunctionalComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace VDStudios.MagicEngine.Internal;
+
+/// <summary>
+/// Internal use only
+/// </summary>
+public interface IFunctionalComponent
+{
+    /// <summary>
+    /// Internal use only
+    /// </summary>
+    public void InternalDetach();
+
+    /// <summary>
+    /// Internal use only
+    /// </summary>
+    public int Index { get; }
+}
