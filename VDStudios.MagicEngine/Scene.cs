a06eb8f69eb7fd9f55959b9f517f7a55190c1a75
Diego García <dagarciam1014@gmail.com>
2022-06-16 01:03:22 -0400

    Update Scene.cs

--- VDStudios.MagicEngine/Scene.cs
+++ VDStudios.MagicEngine/Scene.cs
@@ -1,5 +1,44 @@
 ﻿namespace VDStudios.MagicEngine;
 
+/// <summary>
+/// Represents a general state of the game
+/// </summary>
+/// <remarks>
+/// A scene can be the MainMenu, a Room, a Dungeon, and any number of things. It is a self-contained state of the <see cref="Game"/>
+/// </remarks>
 public class Scene
 {
+    /// <summary>
+    /// Instances a new Scene
+    /// </summary>
+    public Scene()
+    {
+        Game = Game.Instance;
+    }
+
+    /// <summary>
+    /// The <see cref="MagicEngine.Game"/> this <see cref="Scene"/> belongs to. Same as<see cref="SDL2.NET.SDLApplication{TApp}.Instance"/>
+    /// </summary>
+    protected Game Game { get; }
+
+    internal Task InternalUpdate(TimeSpan delta);
+    internal void InternalDraw(TimeSpan delta);
+    internal Task InternalBegin(IServiceProvider services);
+    internal Task InternalEnd(Scene next);
+
+    protected virtual Task Update(TimeSpan delta);
+
+    #region Events
+
+    /// <summary>
+    /// Fired when the <see cref="Scene"/> begins and is set on <see cref="Game.CurrentScene"/>
+    /// </summary>
+    public event SceneEvent? SceneBegan;
+
+    /// <summary>
+    /// Fired when the <see cref="Scene"/> ends and is withdrawn from <see cref="Game.CurrentScene"/>
+    /// </summary>
+    public event SceneEvent? SceneEnded;
+
+    #endregion
 }
