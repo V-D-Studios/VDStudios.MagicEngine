3a665f33df6c2ddc6d49476504f28f6ca013800d
Diego García <dagarciam1014@gmail.com>
2022-06-16 20:22:18 -0400

    Create NodeList.cs

---
+++ VDStudios.MagicEngine/NodeList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// Represents a list of nodes in a <see cref="Node"/> or <see cref="Scene"/>
+/// </summary>
+public sealed class NodeList
+{
+}
