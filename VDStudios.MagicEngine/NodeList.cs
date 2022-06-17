cb09c503d5d2b724b283d7b430d876a17ae17eb1
Diego García <dagarciam1014@gmail.com>
2022-06-16 20:23:34 -0400

    Update NodeList.cs

--- VDStudios.MagicEngine/NodeList.cs
+++ VDStudios.MagicEngine/NodeList.cs
@@ -9,6 +9,10 @@ namespace VDStudios.MagicEngine;
 /// <summary>
 /// Represents a list of nodes in a <see cref="Node"/> or <see cref="Scene"/>
 /// </summary>
+/// <remarks>
+/// This class cannot be inherited. This class cannot be instanced by user code
+/// </remarks>
 public sealed class NodeList
 {
+    internal NodeList() { }
 }
