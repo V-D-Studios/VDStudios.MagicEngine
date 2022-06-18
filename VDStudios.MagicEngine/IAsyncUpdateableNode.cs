e09690bcc930502ba69243405ab72d64b296d926
Diego García <dagarciam1014@gmail.com>
2022-06-17 23:01:49 -0400

    Update IAsyncUpdateableNode.cs

--- VDStudios.MagicEngine/IAsyncUpdateableNode.cs
+++ VDStudios.MagicEngine/IAsyncUpdateableNode.cs
@@ -1,15 +1,15 @@
 ﻿namespace VDStudios.MagicEngine;
 
 /// <summary>
-/// Represents a <see cref="Node"/> or <see cref="FunctionalComponent{TNode}"/> that is to be updated asynchronously. Takes precedence over <see cref="IUpdateableNode"/> if both are implemented
+/// Represents a <see cref="Node"/> that is to be updated asynchronously. Takes precedence over <see cref="IUpdateableNode"/> if both are implemented
 /// </summary>
 public interface IAsyncUpdateableNode
 {
     /// <summary>
-    /// Updates the <see cref="Node"/> or <see cref="FunctionalComponent{TNode}"/> asynchronously
+    /// Updates the <see cref="Node"/> asynchronously
     /// </summary>
     /// <param name="delta">The amount of time that has passed since the last update batch call</param>
-    public ValueTask UpdateAsync(TimeSpan delta);
+    public Task UpdateAsync(TimeSpan delta);
 
     /// <summary>
     /// The batch this <see cref="IUpdateableNode"/> should be assigned to
