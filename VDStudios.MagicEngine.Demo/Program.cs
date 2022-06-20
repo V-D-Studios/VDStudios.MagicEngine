d7dcad5bf59358b92744d6612daa42c6ed469189
Diego García <dagarciam1014@gmail.com>
2022-06-19 23:02:09 -0400

    Added a small demo of MagicEngine

---
+++ VDStudios.MagicEngine.Demo/Program.cs
@@ -0,0 +1,5 @@
+﻿using SDL2.NET;
+using VDStudios.MagicEngine;
+using VDStudios.MagicEngine.Demo.Scenes;
+
+await SDLAppBuilder.CreateInstance<Game>().StartGame<DemoScene>();
--- VDStudios.MagicEngine.Demo/Scenes/DemoScene.cs
+++ VDStudios.MagicEngine.Demo/Scenes/DemoScene.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VDStudios.MagicEngine.Demo.Nodes;
 
 namespace VDStudios.MagicEngine.Demo.Scenes;
 public sealed class DemoScene : Scene
 {
+    public DemoScene()
+    {
+        AttachNode(new PlayerNode());
+    }
 }
