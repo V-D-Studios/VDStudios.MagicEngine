5cfe3e6ceeead47032b3d9caf863a2647c5d26c4
Diego García <dagarciam1014@gmail.com>
2022-06-26 02:55:22 -0400

    Created new Exceptions

---
+++ VDStudios.MagicEngine/Exceptions/WindowChangeRejectedException.cs
@@ -0,0 +1,23 @@
+﻿using SDL2.NET;
+
+namespace VDStudios.MagicEngine.Exceptions;
+
+/// <summary>
+/// An exception that is thrown when a <see cref="GraphicsManager"/> rejects the changing of its <see cref="GraphicsManager.Window"/>
+/// </summary>
+[Serializable]
+public class WindowChangeRejectedException : Exception
+{
+    /// <summary>
+    /// Instances and describes a new <see cref="WindowChangeRejectedException"/>
+    /// </summary>
+    /// <param name="reason">The reason the <see cref="Window"/> change was rejected</param>
+    public WindowChangeRejectedException(string? reason)
+        : base($"This GraphicsManager rejected its Window being changed{(reason is null ? "" : $": {reason}")}")
+    { }
+
+    /// <inheritdoc/>
+    protected WindowChangeRejectedException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
