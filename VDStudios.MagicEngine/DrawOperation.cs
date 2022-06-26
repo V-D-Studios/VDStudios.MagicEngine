c3d2c43751af3e4d60a9ff9b937a5d8999371539
Diego García <dagarciam1014@gmail.com>
2022-06-26 02:55:51 -0400

    Partially implemented DrawOperation

--- VDStudios.MagicEngine/DrawOperation.cs
+++ VDStudios.MagicEngine/DrawOperation.cs
@@ -1,19 +1,42 @@
 ﻿using SDL2.NET;
 using System.Numerics;
+using Veldrid;
 
 namespace VDStudios.MagicEngine;
 
 /// <summary>
 /// Represents an operation that is ready to be drawn
 /// </summary>
-public abstract class DrawOperation : IDisposable
+/// <remarks>
+/// Try to keep an object created from this class cached somewhere in a node, as they incur a number of allocations that should be avoided in a HotPath like the rendering sequence
+/// </remarks>
+public abstract class DrawOperation
 {
+    protected DrawOperation(GraphicsDevice device)
+    {
+        Device = device;
+        Commands = device.ResourceFactory.CreateCommandList();
+    }
+
+    internal CommandList Commands;
+    internal GraphicsDevice Device;
+
+    internal void InternalDraw(Vector2 offset, GraphicsDevice device)
+    {
+        if (!ReferenceEquals(device, Device))
+            throw new InvalidOperationException($"Can't perform a Draw operation on a different device than this operation was created with");
+        Commands.Begin();
+        Draw(offset, Commands, device);
+        Commands.End();
+    }
+
     /// <summary>
     /// The method that will be used to draw the component
     /// </summary>
     /// <param name="offset">The translation offset of the drawing operation</param>
-    /// <param name="renderer">The SDL2 renderer</param>
-    protected internal abstract void Draw(Vector2 offset, Renderer renderer);
+    /// <param name="device">The Veldrid <see cref="GraphicsDevice"/></param>
+    /// <param name="commandList">The <see cref="CommandList"/> opened specifically for this call. <see cref="CommandList.End"/> will be called AFTER this method returns, so don't call it yourself</param>
+    protected abstract void Draw(Vector2 offset, CommandList commandList, GraphicsDevice device);
 
     /// <summary>
     /// This method is called automatically when this <see cref="DrawOperation"/> is going to be drawn for the first time
