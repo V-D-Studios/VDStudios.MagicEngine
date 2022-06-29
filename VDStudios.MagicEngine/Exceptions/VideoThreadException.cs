b5bf29e07aaf37ecb5d2dd07e4f809e01c3cd08f
Diego García <dagarciam1014@gmail.com>
2022-06-28 22:16:02 -0400

    Create VideoThreadException.cs

---
+++ VDStudios.MagicEngine/Exceptions/VideoThreadException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine.Exceptions;
+
+/// <summary>
+/// An exception that is thrown when the <see cref="Game"/>'s video thread throws an exception
+/// </summary>
+/// <remarks>
+/// This class cannot be inherited or instanced by user code
+/// </remarks>
+[Serializable]
+public sealed class VideoThreadException : Exception
+{
+    internal VideoThreadException(Exception exception) : base("The Game's VideoThread has thrown an exception and can no longer perform any work", exception) { }
+}
