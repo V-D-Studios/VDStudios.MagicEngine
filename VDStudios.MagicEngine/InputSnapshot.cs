120f0eccc123db71df588955c28e6a018d89df9c
Diego García <dagarciam1014@gmail.com>
2022-07-01 15:55:57 -0400

    Update InputSnapshot.cs

--- VDStudios.MagicEngine/InputSnapshot.cs
+++ VDStudios.MagicEngine/InputSnapshot.cs
@@ -11,10 +11,29 @@ using static SDL2.NET.Window;
 namespace VDStudios.MagicEngine;
 
 /// <summary>
-/// Represents a snapshot of given input at a specific time
+/// Represents a recording of a Key Event
+/// </summary>
+/// <param name="Scancode">The Scancode of the key</param>
+/// <param name="Key">The actual value of the key</param>
+/// <param name="Modifiers">The modifiers that were active at the time of this event</param>
+/// <param name="IsPressed">Whether the key was pressed or not</param>
+/// <param name="Repeat">Whether the key was a repeat press or not</param>
+/// <param name="Unicode">The unicode value of the key</param>
+public readonly record struct KeyEventRecord(Scancode Scancode, Keycode Key, KeyModifier Modifiers, bool IsPressed, bool Repeat, uint Unicode);
+
+/// <summary>
+/// Represents a recording of a Mouse Event
+/// </summary>
+/// <param name="Delta">The amount of travel the Mouse had during this event</param>
+/// <param name="NewPosition">The new position of the mouse</param>
+/// <param name="Pressed">The buttons that were pressed at the time of this event</param>
+public readonly record struct MouseEventRecord(Vector2 Delta, Vector2 NewPosition, MouseButton Pressed);
+
+/// <summary>
+/// Represents a snapshot of given input at a specific frame
 /// </summary>
 /// <remarks>
-/// Disposing this object returns it to the Snapshot pool, not doing so may result in extra allocations
+/// Input events get recorded at all times, and are reset at the beginning of a new frame. Disposing this object returns it to the Snapshot pool, not doing so may result in extra allocations
 /// </remarks>
 public sealed class InputSnapshot : IDisposable
 {
@@ -32,14 +51,20 @@ public sealed class InputSnapshot : IDisposable
     /// <summary>
     /// The key events that happened at the time of this snapshot
     /// </summary>
-    public IReadOnlyList<KeyState> KeyEvents => kEvs;
-    internal List<KeyState> kEvs = new();
+    public IReadOnlyList<KeyEventRecord> KeyEvents => kEvs;
+    internal List<KeyEventRecord> kEvs = new();
 
     /// <summary>
     /// The characters that were pressed at the time of this snapshot
     /// </summary>
-    public IReadOnlyList<char> KeyCharPresses => kcEvs;
-    internal List<char> kcEvs = new();
+    public IReadOnlyList<uint> KeyCharPresses => kcEvs;
+    internal List<uint> kcEvs = new();
+
+    /// <summary>
+    /// The characters that were pressed at the time of this snapshot
+    /// </summary>
+    public IReadOnlyList<MouseEventRecord> MouseEvents => mEvs;
+    internal List<MouseEventRecord> mEvs = new();
 
     /// <summary>
     /// The mouse position at the time of this snapshot
@@ -47,9 +72,14 @@ public sealed class InputSnapshot : IDisposable
     public Vector2 MousePosition { get; internal set; }
 
     /// <summary>
-    /// The mouse wheel's delta at the time of this snapshot
+    /// The mouse wheel's vertical delta at the time of this snapshot
     /// </summary>
-    public float WheelDelta { get; internal set; }
+    public float WheelVerticalDelta { get; internal set; }
+
+    /// <summary>
+    /// The mouse wheel's horizontal delta at the time of this snapshot
+    /// </summary>
+    public float WheelHorizontalDelta { get; internal set; }
 
     /// <summary>
     /// Checks if <paramref name="button"/> was pressed at the time of this snapshot
@@ -71,10 +101,32 @@ public sealed class InputSnapshot : IDisposable
     internal void Clear()
     {
         butt = 0;
-        WheelDelta = 0;
+        WheelHorizontalDelta = 0;
+        WheelVerticalDelta = 0;
         MousePosition = default;
         kEvs.Clear();
         kcEvs.Clear();
+        mEvs.Clear();
+    }
+
+    internal void CopyTo(InputSnapshot other)
+    {
+        other.butt = butt;
+        other.WheelHorizontalDelta = WheelHorizontalDelta;
+        other.WheelVerticalDelta = WheelVerticalDelta;
+        other.MousePosition = MousePosition;
+
+        other.kEvs.EnsureCapacity(kEvs.Capacity);
+        for (int i = 0; i < kEvs.Count; i++)
+            other.kEvs[i] = kEvs[i];
+
+        other.kcEvs.EnsureCapacity(kcEvs.Capacity);
+        for (int i = 0; i < kcEvs.Count; i++)
+            other.kcEvs[i] = kcEvs[i];
+
+        other.mEvs.EnsureCapacity(mEvs.Capacity);
+        for (int i = 0; i < mEvs.Count; i++)
+            other.mEvs[i] = mEvs[i];
     }
 
     void IDisposable.Dispose() => ReturnToPool();
