27a540bc49eb92f86ce31e712b10c5b9e9219fb9
Diego García <dagarciam1014@gmail.com>
2022-07-03 17:03:38 -0400

    Update Usings.cs

--- VDStudios.MagicEngine/Usings.cs
+++ VDStudios.MagicEngine/Usings.cs
@@ -3,4 +3,5 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+global using VDStudios.MagicEngine.Properties;
 global using Texture = Veldrid.Texture;
