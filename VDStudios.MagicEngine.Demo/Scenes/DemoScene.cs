4a5232c62f1d47f5b2d2e93f28b014023f517a0e
Diego García <dagarciam1014@gmail.com>
2022-07-03 21:53:22 -0400

    Update DemoScene.cs

--- VDStudios.MagicEngine.Demo/Scenes/DemoScene.cs
+++ VDStudios.MagicEngine.Demo/Scenes/DemoScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VDStudios.MagicEngine.Demo.GUI.Elements;
 using VDStudios.MagicEngine.Demo.Nodes;
 
 namespace VDStudios.MagicEngine.Demo.Scenes;
@@ -17,5 +18,28 @@ public sealed class DemoScene : Scene
     {
         await Attach(new ColorBackgroundNode());
         //await Attach(new PlayerNode());
+
+        // The following code can (and should) go somewhere else
+        int top = Random.Shared.Next(0, 6);
+        int mid = Random.Shared.Next(0, 6);
+        int bot = Random.Shared.Next(0, 6);
+
+        for (int t = 0; t < top; t++)
+        {
+            var tel = new TestElement();
+            Game.MainGraphicsManager.AddElement(tel);
+            
+            for (int m = 0; m < mid; m++)
+            {
+                var mel = new TestElement();
+                tel.AddElement(mel);
+
+                for (int b = 0; b < bot; b++)
+                {
+                    var bel = new TestElement();
+                    mel.AddElement(bel);
+                }
+            }
+        }
     }
 }
