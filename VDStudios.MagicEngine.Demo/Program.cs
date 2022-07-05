17243b53b1c9b4eb24662855f028a241c044f6d5
Diego García <dagarciam1014@gmail.com>
2022-07-05 16:27:02 -0400

    Update Program.cs

--- VDStudios.MagicEngine.Demo/Program.cs
+++ VDStudios.MagicEngine.Demo/Program.cs
@@ -3,3 +3,8 @@ using VDStudios.MagicEngine;
 using VDStudios.MagicEngine.Demo.Scenes;
 
 await Game.Instance.StartGame<DemoScene>();
+
+#if !DEBUG
+Console.WriteLine("\nPress any key to continue...");
+Console.ReadKey();
+#endif
