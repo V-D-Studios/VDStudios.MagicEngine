42bf7961a9da4adab3d30afa1c7a4df3a34cddac
Diego García <dagarciam1014@gmail.com>
2022-07-08 00:56:13 -0400

    Update UpdateBatchCollection.cs

--- VDStudios.MagicEngine/Internal/UpdateBatchCollection.cs
+++ VDStudios.MagicEngine/Internal/UpdateBatchCollection.cs
@@ -9,9 +9,9 @@ internal sealed class UpdateBatchCollection
 {
     public const int BatchCount = (int)UpdateBatch.Last + 1;
 
-    private readonly Dictionary<int, Node>?[] Batches = new Dictionary<int, Node>?[BatchCount];
+    private readonly UpdateSynchronicityBatch[] Batches = new UpdateSynchronicityBatch[BatchCount];
 
-    public Dictionary<int, Node>.ValueCollection? this[UpdateBatch batch]
+    public Dictionary<int, Node>.ValueCollection? this[UpdateBatch batch, AsynchronousTendency tendency]
     {
         get
         {
@@ -19,33 +19,32 @@ internal sealed class UpdateBatchCollection
                 throw new InvalidOperationException("Indexed batch is set to -1, which means it's unset. This is likely a library bug.");
 
             lock (Batches)
-                return Batches[(int)batch]?.Values;
+                return Batches[(int)batch] is UpdateSynchronicityBatch usb ? usb[tendency] : null;
         }
     }
 
-    public void Add(Node node, UpdateBatch assignation)
+    public void Add(Node node, UpdateBatch batch, AsynchronousTendency tendency)
     {
-        if (assignation is (UpdateBatch)(-1))
+        if (batch is (UpdateBatch)(-1))
             throw new InvalidOperationException("Node assignation is set to -1, which means it's unset. This is likely a library bug.");
         lock (Batches)
         {
-            var batch = Batches[(int)assignation];
-            if (batch is null) Batches[(int)assignation] = batch = new(3);
-            batch.Add(node.Id, node);
+            var _batch = Batches[(int)batch];
+            if (_batch is null) Batches[(int)tendency] = _batch = new();
+            _batch.Add(node, tendency);
         }
     }
 
-    public void Remove(Node node, UpdateBatch assignation)
+    public void Remove(Node node, UpdateBatch batch, AsynchronousTendency tendency)
     {
-        if (assignation is (UpdateBatch)(-1))
+        if (batch is (UpdateBatch)(-1))
             throw new InvalidOperationException("Node assignation is set to -1, which means it's unset. This is likely a library bug.");
         lock (Batches)
         {
-            var batch = Batches[(int)assignation];
-            if (batch is null)
-                throw new InvalidOperationException($"Batch \"{assignation}\" is empty and cannot have any nodes removed from it. This is likely a library bug.");
-            if (!batch.Remove(node.Id))
-                throw new InvalidOperationException($"Could not remove node of id {node.Id} from batch \"{assignation}\"; is this the correct batch in which it was registered? This is likely a library bug.");
+            var _batch = Batches[(int)batch];
+            if (_batch is null)
+                throw new InvalidOperationException($"Batch \"{batch}\" is empty and cannot have any nodes removed from it. This is likely a library bug.");
+            _batch.Remove(node, tendency);
         }
     }
 }
