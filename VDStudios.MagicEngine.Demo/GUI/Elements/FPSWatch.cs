4b1f01fef3b189aa549edc6855d8a90aa802f7f5
Diego García <dagarciam1014@gmail.com>
2022-07-08 15:54:43 -0400

    Improved memory management of FPSWatch

--- VDStudios.MagicEngine.Demo/GUI/Elements/FPSWatch.cs
+++ VDStudios.MagicEngine.Demo/GUI/Elements/FPSWatch.cs
@@ -10,19 +10,32 @@ namespace VDStudios.MagicEngine.Demo.GUI.Elements;
 
 public sealed class FPSWatch : GUIElement
 {
-    private static readonly ConcurrentDictionary<long, string> strings;
-
-    static FPSWatch()
+    private static readonly WeakReference<ConcurrentDictionary<long, string>> strings = new(CreateDict());
+    private static ConcurrentDictionary<long, string> CreateDict()
     {
-        strings = new(5, 100);
-        strings[1] = "1 frame per second";
+        ConcurrentDictionary<long, string> dict = new(1, 100);
+        dict[1] = "1 frame per second";
+        return dict;
+    }
+
+    private readonly ConcurrentDictionary<long, string> strs;
+
+    public FPSWatch()
+    {
+        lock (strings)
+            if (!strings.TryGetTarget(out strs!))
+            {
+                var d = CreateDict();
+                strings.SetTarget(d);
+                strs = d;
+            }
     }
 
     protected override void SubmitUI(TimeSpan delta, IReadOnlyCollection<GUIElement> subElements)
     {
         var fps = (long)Manager!.FramesPerSecond;
         ImGui.Begin("Frames Per Second");
-        ImGui.Text(strings.GetOrAdd(fps, GenStr));
+        ImGui.Text(strs.GetOrAdd(fps, GenStr));
         ImGui.End();
     }
 
