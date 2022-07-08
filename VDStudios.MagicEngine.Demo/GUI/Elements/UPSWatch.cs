3abcbd0a8ae7f68034732a1376b51df2b13d7d19
Diego García <dagarciam1014@gmail.com>
2022-07-08 01:55:04 -0400

    Update UPSWatch.cs

--- VDStudios.MagicEngine.Demo/GUI/Elements/UPSWatch.cs
+++ VDStudios.MagicEngine.Demo/GUI/Elements/UPSWatch.cs
@@ -6,21 +6,34 @@ namespace VDStudios.MagicEngine.Demo.GUI.Elements;
 
 public sealed class UPSWatch : GUIElement
 {
-    private static readonly ConcurrentDictionary<long, string> strings;
+    private static readonly ConcurrentDictionary<long, string> tickStrings;
+    private static readonly ConcurrentDictionary<double, string> msStrings;
+    private static readonly ConcurrentDictionary<double, string> upsStrings;
 
     static UPSWatch()
     {
-        strings = new(5, 100);
-        strings[1] = "1 tick";
+        tickStrings = new(5, 100);
+        tickStrings[1] = "1 tick";
+
+        msStrings = new(5, 100);
+        msStrings[1] = "1ms";
+
+        upsStrings = new(5, 100);
+        upsStrings[1] = "1 update per second";
     }
 
     protected override void SubmitUI(TimeSpan delta, IReadOnlyCollection<GUIElement> subElements)
     {
-        var fps = Game.AverageDelta.Ticks;
-        ImGui.Begin("Ticks per update");
-        ImGui.Text(strings.GetOrAdd(fps, GenStr));
+        var tpu = Game.AverageDelta.Ticks;
+        var mspu = Game.AverageDelta.TotalMilliseconds;
+        var ups = Math.Truncate(1 / Game.AverageDelta.TotalSeconds);
+
+        ImGui.Begin("Update metrics");
+        ImGui.Text(tickStrings.GetOrAdd(tpu, GenStr, " ticks"));
+        ImGui.Text(msStrings.GetOrAdd(mspu, GenStr, "ms"));
+        ImGui.Text(upsStrings.GetOrAdd(ups, GenStr, " updates per second"));
         ImGui.End();
     }
 
-    private string GenStr(long fps) => $"{fps:#.##} ticks";
+    private string GenStr<TInt>(TInt fps, string metric) => $"{fps:#.##}{metric}";
 }
