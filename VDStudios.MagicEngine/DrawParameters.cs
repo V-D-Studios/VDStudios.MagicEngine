03bdb430dae6c9e836850e2e4bb63d7de4d3592c
Diego García <dagarciam1014@gmail.com>
2022-07-09 14:36:44 -0400

    Create DrawParameters.cs

---
+++ VDStudios.MagicEngine/DrawParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// Provides parameters and other useful data to be used when drawing in a <see cref="DrawOperation"/>
+/// </summary>
+/// <param name="Transform">The transformation matrix in these parameters</param>
+public readonly record struct DrawParameters(Matrix4x4 Transform);
