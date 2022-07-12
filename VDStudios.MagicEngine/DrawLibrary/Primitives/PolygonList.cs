5f57eaac39e2cc528fd891ff4cf76bcddab44191
Diego García <dagarciam1014@gmail.com>
2022-07-12 16:28:47 -0400

    Update PolygonList.cs

--- VDStudios.MagicEngine/DrawLibrary/Primitives/PolygonList.cs
+++ VDStudios.MagicEngine/DrawLibrary/Primitives/PolygonList.cs
@@ -1,22 +1,42 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Veldrid;
+using Veldrid.SPIRV;
 
 namespace VDStudios.MagicEngine.DrawLibrary.Primitives;
 
 /// <summary>
-/// Represents a 2D shape with arbitrary sides
+/// Represents an operation to draw a list of 2D shapes with arbitrary sides
 /// </summary>
 /// <remarks>
-/// Not to be confused with Polyhedron, a 3D shape with arbitrary faces
+/// Not to be confused with Polyhedron, a 3D shape
 /// </remarks>
 public class PolygonList : DrawOperation, IList<PolygonDefinition>
 {
-    private readonly List<PolygonDefinition> _polygons = new();
+    private readonly List<PolygonDefinition> _polygons;
+
+    private ShaderDescription VertexShaderDesc;
+    private ShaderDescription FragmentShaderDesc;
+
+    /// <summary>
+    /// Instantiates a new <see cref="PolygonList"/>
+    /// </summary>
+    /// <param name="polygons">The polygons to fill this list with</param>
+    /// <param name="fragmentShaderSpirv">The description of this <see cref="PolygonList"/>'s Fragment Shader in Vulkan style GLSL or SPIR-V bytecode; or <c>null</c> to use the default</param>
+    /// <param name="vertexShaderSpirv">The description of this <see cref="PolygonList"/>'s Vertex Shader in Vulkan style GLSL or SPIR-V bytecode; or <c>null</c> to use the default</param>
+    public PolygonList(IEnumerable<PolygonDefinition> polygons, ShaderDescription? vertexShaderSpirv = null, ShaderDescription? fragmentShaderSpirv = null)
+    {
+        _polygons = new(polygons);
+        VertexShaderDesc = vertexShaderSpirv ?? new ShaderDescription(ShaderStages.Vertex, BuiltInResources.DefaultPolygonVertexShader, "main");
+        FragmentShaderDesc = fragmentShaderSpirv ?? new ShaderDescription(ShaderStages.Fragment, BuiltInResources.DefaultPolygonFragmentShader, "main");
+    }
 
     #region List
 
