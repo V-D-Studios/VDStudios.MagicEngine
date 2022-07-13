a0c2f3e0ef1d815386a30f4eda3c9b332e3af5e9
Diego García <dagarciam1014@gmail.com>
2022-07-13 00:42:57 -0400

    Create FloatingShapesNode.cs

---
+++ VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using VDStudios.MagicEngine.DrawLibrary.Primitives;
+
+namespace VDStudios.MagicEngine.Demo.Nodes;
+public class FloatingShapesNode : Node, IDrawableNode
+{
+    public FloatingShapesNode()
+    {
+        float toffset = 1;
+        float roffset = -1;
+
+        Span<Vector2> triangle = stackalloc Vector2[] { new(-.5f + toffset, -.5f + toffset), new(.5f + toffset, -.5f + toffset), new(toffset, .5f + toffset) };
+        Span<Vector2> rectangle = stackalloc Vector2[] { new(-1f + roffset, -.5f + roffset), new(-1f + roffset, .5f + roffset), new(1f + roffset, .5f + roffset), new(1f + roffset, -.5f + roffset) };
+
+        DrawOperationManager = new(this);
+        DrawOperationManager.AddDrawOperation(new PolygonList(new PolygonDefinition[]
+        {
+            new(triangle),
+            new(rectangle)
+        }));
+    }
+
+    public DrawOperationManager DrawOperationManager { get; }
+
+    public bool SkipDrawPropagation { get; }
+}
