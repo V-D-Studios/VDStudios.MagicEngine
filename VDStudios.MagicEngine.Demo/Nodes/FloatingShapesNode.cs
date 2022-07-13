ac3df07553f7942fdb483f3d57c37fc5a6720653
Diego García <dagarciam1014@gmail.com>
2022-07-13 19:09:16 -0400

    Update FloatingShapesNode.cs

--- VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
+++ VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
@@ -11,11 +11,11 @@ public class FloatingShapesNode : Node, IDrawableNode
 {
     public FloatingShapesNode()
     {
-        float toffset = 1;
-        float roffset = -1;
+        float toffset = .25f;
+        float roffset = -.25f;
 
-        Span<Vector2> triangle = stackalloc Vector2[] { new(-.5f + toffset, -.5f + toffset), new(.5f + toffset, -.5f + toffset), new(toffset, .5f + toffset) };
-        Span<Vector2> rectangle = stackalloc Vector2[] { new(-1f + roffset, -.5f + roffset), new(-1f + roffset, .5f + roffset), new(1f + roffset, .5f + roffset), new(1f + roffset, -.5f + roffset) };
+        Span<Vector2> triangle = stackalloc Vector2[] { new(-.25f + toffset, -.25f + toffset), new(.25f + toffset, -.25f + toffset), new(toffset, .25f + toffset) };
+        Span<Vector2> rectangle = stackalloc Vector2[] { new(-.25f + roffset, -.25f + roffset), new(-.5f + roffset, .25f + roffset), new(.5f + roffset, .25f + roffset), new(.5f + roffset, -.25f + roffset) };
 
         DrawOperationManager = new DrawOperationManagerDrawQueueDelegate(this, (q, o) =>
         {
