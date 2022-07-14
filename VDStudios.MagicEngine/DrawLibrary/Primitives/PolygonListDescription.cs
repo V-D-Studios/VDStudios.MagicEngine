057224fb8dad4d0e67279d4008c46240bd273e66
Diego García <dagarciam1014@gmail.com>
2022-07-14 02:24:46 -0400

    Create PolygonListDescription.cs

---
+++ VDStudios.MagicEngine/DrawLibrary/Primitives/PolygonListDescription.cs
@@ -0,0 +1,14 @@
+﻿using Veldrid;
+
+namespace VDStudios.MagicEngine.DrawLibrary.Primitives;
+
+/// <summary>
+/// Represents a description to configure a <see cref="PolygonList"/>
+/// </summary>
+public struct PolygonListDescription
+{
+    /// <summary>
+    /// Describes how the polygons for the destination <see cref="PolygonList"/> will be filled
+    /// </summary>
+    public PolygonFillMode FillMode;
+}
