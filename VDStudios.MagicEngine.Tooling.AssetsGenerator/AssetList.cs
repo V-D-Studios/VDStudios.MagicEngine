e7f8c2658e80dccf29e2be186fcac43e8cd42022
Diego García <dagarciam1014@gmail.com>
2022-07-13 22:19:58 -0400

    Create AssetList.cs

---
+++ VDStudios.MagicEngine.Tooling.AssetsGenerator/AssetList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static VDStudios.MagicEngine.Tooling.AssetsGenerator.Helper;
+
+namespace VDStudios.MagicEngine.Tooling.AssetsGenerator;
+
+internal sealed class AssetGroup
+{
+    public string GroupName
+    {
+        get => PropGet(ref gn)!;
+        init => PropSet(ref gn, value);
+    }
+    private string? gn;
+
+    public AssetDescription[] Assets { get; init; } = Array.Empty<AssetDescription>();
+}
+
+internal sealed class AssetDescription
+{
+
+    public string File
+    {
+        get => PropGet(ref fn)!;
+        init => PropSet(ref fn, value);
+    }
+    string? fn;
+
+    public string Name
+    {
+        get => n ??= FileToName(File);
+        init => n = value;
+    }
+    string? n;
+
+    public string ReadAs
+    {
+        get => ReadAsValue.ToString();
+        init => ReadAsValue = Enum.Parse<ReadAs>(value);
+    }
+
+    internal ReadAs ReadAsValue { get; init; }
+}
+
+public enum ReadAs
+{
+    RawBytes,
+    UTF8Text
+}
