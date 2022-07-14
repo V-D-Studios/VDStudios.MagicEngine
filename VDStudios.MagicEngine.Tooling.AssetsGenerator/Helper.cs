2a9846739797ad9367f6d00a0d553681232fc9ec
Diego García <dagarciam1014@gmail.com>
2022-07-13 22:19:55 -0400

    Create Helper.cs

---
+++ VDStudios.MagicEngine.Tooling.AssetsGenerator/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace VDStudios.MagicEngine.Tooling.AssetsGenerator;
+
+internal static class Helper
+{
+    public readonly static Regex CleanupRegex = new(@"^\d+|{|}|;|\.|,|!|\n", RegexOptions.Compiled);
+    public static string FileToName(string file)
+    {
+        var f = file;
+        while (CleanupRegex.IsMatch(f))
+        {
+            f = CleanupRegex.Replace(f, "");
+            if (f == "")
+                throw new InvalidOperationException($"The filename is an invalid C# Member name and must be set explicitly");
+        }
+        return f;
+    }
+
+    public static void PropSet<T>(ref T v, T value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        v = value;
+    }
+    public static T PropGet<T>(ref T v, [CallerMemberName] string? caller = null)
+        => v ?? throw new InvalidOperationException($"This asset does not have a {caller}");
+
+}
