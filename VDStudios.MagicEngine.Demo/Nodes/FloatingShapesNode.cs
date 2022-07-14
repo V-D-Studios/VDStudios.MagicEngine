1a46abc47c40aad06f482e8585729e4f5ede1fb5
Diego García <dagarciam1014@gmail.com>
2022-07-13 20:24:17 -0400

    Update FloatingShapesNode.cs

--- VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
+++ VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
@@ -12,7 +12,7 @@ public class FloatingShapesNode : Node, IDrawableNode
     public FloatingShapesNode()
     {
         float toffset = .25f;
-        float roffset = -.25f;
+        float roffset = -.55f;
 
         Span<Vector2> triangle = stackalloc Vector2[] { new(-.25f + toffset, -.25f + toffset), new(.25f + toffset, -.25f + toffset), new(toffset, .25f + toffset) };
         Span<Vector2> rectangle = stackalloc Vector2[] { new(-.25f + roffset, -.25f + roffset), new(-.5f + roffset, .25f + roffset), new(.5f + roffset, .25f + roffset), new(.5f + roffset, -.25f + roffset) };
