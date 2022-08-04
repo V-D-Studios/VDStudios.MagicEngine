fab5f499ceba256e88940035cb8742f39baa69a6
Diego García <dagarciam1014@gmail.com>
2022-08-04 00:59:25 -0400

    Update FloatingShapesNode.cs

--- VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
+++ VDStudios.MagicEngine.Demo/Nodes/FloatingShapesNode.cs
@@ -13,7 +13,7 @@ using Veldrid;
 namespace VDStudios.MagicEngine.Demo.Nodes;
 public class FloatingShapesNode : Node, IDrawableNode
 {
-    CircumferenceDefinition circumference;
+    CircleDefinition circle;
     PolygonDefinition hexagon;
 
     public FloatingShapesNode()
@@ -21,13 +21,13 @@ public class FloatingShapesNode : Node, IDrawableNode
         Span<Vector2> triangle = stackalloc Vector2[] { new(-.15f + .5f, -.15f + .5f), new(.15f + .5f, -.15f + .5f), new(.5f, .15f + .5f) };
         hexagon = new PolygonDefinition(stackalloc Vector2[]
         {
-            new(330.71074380165294f / 500f - 0.5f, 494.82644628099155f / 500f - 0.5f),
-            new(539.801652892562f / 500f - 0.5f, 439.4545454545454f / 500f - 0.5f),
-            new(626.876207061902f / 500f - 0.5f, 241.4568745545897f / 500f - 0.5f),
-            new(526.365491022952f / 500f - 0.5f, 49.92971818522767f / 500f - 0.5f),
-            new(313.956123998003f / 500f - 0.5f, 9.09693153458295f / 500f - 0.5f),
-            new(149.59669171830413f / 500f - 0.5f, 149.7064357876441f / 500f - 0.5f),
-            new(157.05319901188642f / 500f - 0.5f, 365.87640633068054f / 500f - 0.5f)
+            new(330.71074380165294f / 500f, 494.82644628099155f / 500f),
+            new(539.801652892562f / 500f, 439.4545454545454f / 500f),
+            new(626.876207061902f / 500f, 241.4568745545897f / 500f),
+            new(526.365491022952f / 500f, 49.92971818522767f / 500f),
+            new(313.956123998003f / 500f, 9.09693153458295f / 500f),
+            new(149.59669171830413f / 500f, 149.7064357876441f / 500f),
+            new(157.05319901188642f / 500f, 365.87640633068054f / 500f)
         }, true) { Name = "Hexagon" };
 
         // Apparently, oddly numbered polygons have their last vertex skipped?
@@ -43,13 +43,21 @@ public class FloatingShapesNode : Node, IDrawableNode
         var circ = PolygonDefinition.Circle(new(-.2f, .15f), .3f, 5);
         circ.Name = "Circle";
 
-        circumference = new CircumferenceDefinition(new(-.7f, .6f), .65f);
+        circle = new CircleDefinition(Vector2.Zero, .65f);
 
         var watch = new Watch("Circle division watch", new()
         {
-            ([NotNullWhen(true)] out string? x) =>
+            new Watch.DelegateViewer(([NotNullWhen(true)] out string? x) =>
             {
-                x = circumference.Subdivisions.ToString();
+                x = circle.Subdivisions.ToString();
+                return true;
+            })
+        },
+        new()
+        {
+            () =>
+            {
+                Log.Debug("Flagged for circle subdivided in {parts} parts", circle.Subdivisions);
                 return true;
             }
         });
@@ -70,9 +78,9 @@ public class FloatingShapesNode : Node, IDrawableNode
     }
 
     TimeSpan tb;
-    static readonly TimeSpan tb_ceil = TimeSpan.FromSeconds(3);
+    static readonly TimeSpan tb_ceil = TimeSpan.FromSeconds(1.5);
     int x = 0;
-    readonly int[] SubDivSeq = new int[] { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6 };
+    readonly int[] SubDivSeq = Enumerable.Range(3, 60).ToArray();
     Vector2 offset;
     float xoff = .01f;
     protected override ValueTask<bool> Updating(TimeSpan delta)
@@ -92,7 +100,7 @@ public class FloatingShapesNode : Node, IDrawableNode
             tb = default;
             if (x >= SubDivSeq.Length)
                 x = 0;
-            circumference.Subdivisions = SubDivSeq[x++];
+            circle.Subdivisions = SubDivSeq[x++];
         }
 
         return ValueTask.FromResult(true);
