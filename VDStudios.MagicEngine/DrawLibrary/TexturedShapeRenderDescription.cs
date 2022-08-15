f622039b8de224214e4978f0711a8fd954d8a7ea
Diego García <dagarciam1014@gmail.com>
2022-08-15 00:16:22 -0400

    Create TexturedShapeRenderDescription.cs

---
+++ VDStudios.MagicEngine/DrawLibrary/TexturedShapeRenderDescription.cs
@@ -0,0 +1,20 @@
+﻿using VDStudios.MagicEngine.DrawLibrary.Geometry;
+using Veldrid;
+
+namespace VDStudios.MagicEngine.DrawLibrary;
+
+/// <summary>
+/// Represents a description to configure
+/// </summary>
+public readonly struct TexturedShapeRenderDescription
+{
+    /// <summary>
+    /// The Description for the backing <see cref="ShapeRenderer{TVertex}"/>
+    /// </summary>
+    public ShapeRendererDescription ShapeRenderer { get; }
+
+    /// <summary>
+    /// The description for the Sampler
+    /// </summary>
+    public SamplerDescription Sampler { get; }
+}
