29997854393fac500b9d1069fc72a10d2656b263
Diego García <dagarciam1014@gmail.com>
2022-09-03 18:21:01 -0400

    Added GetTypeName

--- VDStudios.MagicEngine/AssortedExtensions.cs
+++ VDStudios.MagicEngine/AssortedExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using SDL2.NET;
 using System.Numerics;
+using System.Reflection;
 
 namespace VDStudios.MagicEngine;
 
@@ -8,6 +9,13 @@ namespace VDStudios.MagicEngine;
 /// </summary>
 public static class AssortedExtensions
 {
+    /// <summary>
+    /// This method serves a shorthand for calling <see cref="object.GetType"/> on <paramref name="obj"/> and taking <see cref="MemberInfo.Name"/>
+    /// </summary>
+    /// <param name="obj">The object to get the type name from</param>
+    /// <returns>The type name of <paramref name="obj"/></returns>
+    public static string GetTypeName(this object obj) => obj.GetType().Name;
+
     /// <summary>
     /// Converts a <see cref="Vector2"/> into an SDL.NET <see cref="FPoint"/>
     /// </summary>
