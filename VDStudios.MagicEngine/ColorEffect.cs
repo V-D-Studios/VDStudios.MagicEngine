32765d04c51107ed1c315a9e80fc673b3f2a205f
Diego García <dagarciam1014@gmail.com>
2022-09-20 00:41:57 -0400

    Update ColorEffect.cs

--- VDStudios.MagicEngine/ColorEffect.cs
+++ VDStudios.MagicEngine/ColorEffect.cs
@@ -3,15 +3,23 @@
 /// <summary>
 /// A bitmask representing the color effects for a <see cref="ColorTransformation"/>
 /// </summary>
-public enum ColorEffect : byte
+public enum ColorEffect : uint
 {
+    /// <summary>
+    /// Translates all fragment colors into grayscale space
+    /// </summary>
+    GrayScale = 1 << 0,
+
     /// <summary>
     /// Enables <see cref="ColorTransformation.Tint"/> to be applied over fragments
     /// </summary>
-    Tinted = 1 << 0,
+    /// <remarks>
+    /// Works best if used with <see cref="GrayScale"/>
+    /// </remarks>
+    Tinted = 1 << 1,
 
     /// <summary>
     /// Enables <see cref="ColorTransformation.Overlay"/> to be applied over fragments
     /// </summary>
-    Overlay = 1 << 1,
+    Overlay = 1 << 2,
 }
