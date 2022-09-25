d6754184e2386b2e5f20b4d4971c4727a4e566cf
Diego García <dagarciam1014@gmail.com>
2022-09-25 02:03:24 -0400

    Create Deferred.cs

---
+++ VDStudios.MagicEngine/Deferred.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine;
+public sealed class DeferredExecutionSchedule
+{
+    private static readonly DeferredExecutorList Schedule = new();
+
+    // Schedule in the Game thread
+    //   In {TimeSpan} time
+    //   In n updates
+    //   Recurrent?
+    //   Returning?
+
+    #region Helpers
+
+    private sealed class DeferredExecutorList
+    {
+        public DeferredExecutorNode First;
+        public DeferredExecutorNode Last;
+
+        public void AddLast(DeferredExecutorNode node)
+        {
+            lock (this)
+                Last.Next = node;
+        }
+
+        public void Remove(DeferredExecutorNode node)
+        {
+            lock (this)
+            {
+                var next = node.Next;
+                var prev = node.Previous;
+                if (prev is not null)
+                    prev.Next = next;
+                if (next is not null)
+                    next.Previous = prev;
+            }
+        }
+    }
+
+    private sealed class DeferredExecutorNode
+    {
+        public readonly Action? Action;
+        public readonly TimeSpan Time;
+        public readonly int Frames;
+        public DeferredExecutorNode? Next;
+        public DeferredExecutorNode? Previous;
+    }
+
+    #endregion
+}
