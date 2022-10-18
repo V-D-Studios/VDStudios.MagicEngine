9d95000c81971a1da64b9681d47c7512c78737b8
Diego García <dagarciam1014@gmail.com>
2022-10-18 12:19:04 -0400

    Create ISequenceTimeKeeper.cs

---
+++ VDStudios.MagicEngine/ISequenceTimeKeeper.cs
@@ -0,0 +1,44 @@
+﻿namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// Represents a Time Keeper for a <see cref="TimedSequence{TState}"/>; rules over the measure of time used to advance the sequence
+/// </summary>
+public interface ISequenceTimeKeeper
+{
+    /// <summary>
+    /// The amount of base time each frame is allocated
+    /// </summary>
+    public TimeSpan TimePerFrame { get; }
+
+    /// <summary>
+    /// Queries this keeper about how many frames it should advance
+    /// </summary>
+    /// <remarks>
+    /// If the value returned by this method is greater than 0, the keeper will automatically know to focus on the upcoming frames
+    /// </remarks>
+    /// <param name="currentState">The current state of the sequence</param>
+    /// <param name="frames">The frames in the sequence</param>
+    /// <typeparam name="TState">The type of the state used by a sequence</typeparam>
+    /// <returns>The amount of frames to advance the sequence for</returns>
+    public int QueryAdvance<TState>(int currentState, TimedSequence<TState>.Frame[] frames) where TState : notnull;
+
+    /// <summary>
+    /// Stops keeping time
+    /// </summary>
+    public void Stop();
+
+    /// <summary>
+    /// Starts keeping time
+    /// </summary>
+    public void Start();
+
+    /// <summary>
+    /// Resets the keeper back to its default state
+    /// </summary>
+    public void Reset();
+
+    /// <summary>
+    /// Resets the keeper back to its default state and starts it
+    /// </summary>
+    public void Restart();
+}
