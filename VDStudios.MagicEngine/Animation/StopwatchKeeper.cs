70a620d997d6160ffd2592fa791359f7232c6548
Diego García <dagarciam1014@gmail.com>
2022-10-19 23:12:32 -0400

    Create StopwatchKeeper.cs

---
+++ VDStudios.MagicEngine/Animation/StopwatchKeeper.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace VDStudios.MagicEngine.Animation;
+
+/// <summary>
+/// An <see cref="ISequenceTimeKeeper{TTime}"/> that keeps time using a <see cref="Stopwatch"/>
+/// </summary>
+public class StopwatchSequenceTimeKeeper : ISequenceTimeKeeper<TimeSpan>
+{
+    /// <summary>
+    /// The <see cref="Stopwatch"/> backing the functionality of this object
+    /// </summary>
+    public Stopwatch Watch { get; } = new();
+
+    /// <summary>
+    /// The amount of time each frame can be allocated
+    /// </summary>
+    public TimeSpan TimePerFrame { get; set; }
+
+    private TimeSpan timePerFrame;
+    private TimeSpan correctionOffset;
+
+    /// <summary>
+    /// Constructs a new object of type <see cref="StopwatchSequenceTimeKeeper"/>
+    /// </summary>
+    /// <param name="timePerFrame">The amount of time each frame can be allocated</param>
+    public StopwatchSequenceTimeKeeper(TimeSpan timePerFrame)
+    {
+        this.timePerFrame = TimePerFrame = timePerFrame;
+    }
+
+    /// <inheritdoc/>
+    public int QueryAdvance<TState>(int currentState, TimedSequence<TState, TimeSpan>.Frame[] frames) where TState : notnull
+    {
+        var elapsed = Watch.Elapsed;
+        var t = timePerFrame + frames[currentState].Offset + correctionOffset;
+        int advances = 0;
+        if (elapsed > t)
+        {
+            do
+            {
+                advances++;
+                t += timePerFrame + frames[(currentState + 1) % frames.Length].Offset;
+            } while (elapsed > t);
+
+            Watch.Restart();
+            correctionOffset = t - elapsed;
+            timePerFrame = TimePerFrame;
+        }
+
+        return advances;
+    }
+
+    /// <inheritdoc/>
+    public void Stop()
+        => Watch.Stop();
+
+    /// <inheritdoc/>
+    public void Start()
+        => Watch.Start();
+
+    /// <inheritdoc/>
+    public void Reset()
+        => Watch.Reset();
+
+    /// <inheritdoc/>
+    public void Restart()
+        => Watch.Restart();
+}
