9e415b59375859b09bac93f021d0695e97e27971
Diego García <dagarciam1014@gmail.com>
2022-10-24 22:46:42 -0400

    Update ImGuiManager.cs

--- VDStudios.MagicEngine/Internal/ImGuiManager.cs
+++ VDStudios.MagicEngine/Internal/ImGuiManager.cs
@@ -2,11 +2,13 @@
 
 namespace VDStudios.MagicEngine.Internal;
 
-internal class ImGuiManager
+internal abstract class ImGuiManager : IDisposable
 {
     public readonly ImGuiController Controller;
     public readonly GraphicsManager OwnerManager;
-    private readonly CommandList cl;
+    protected readonly CommandList cl;
+
+    public bool IsActive { get; private set; } = true;
 
     private int widgCount;
     public int WidgetCount => widgCount;
@@ -23,32 +25,134 @@ internal class ImGuiManager
     public void ReportDeadElement()
         => Interlocked.Decrement(ref widgCount);
 
-    public void BeginDraw(InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta)
-    {
-        if (GUIElements.Count > 0) // We check twice, as it may have changed between the first check and the lock being adquired
-        {
-            cl.Begin();
-            cl.SetFramebuffer(gd.SwapchainFramebuffer); // Prepare for ImGUI
-            using (Controller.Begin()) // Lock ImGUI from other GraphicsManagers
-            {
-                foreach (var element in GUIElements)
-                    element.InternalSubmitUI(delta); // Submit UIs
-                Controller.Update(1 / 60f, snapshot);
-                Controller.Render(gd, cl); // Render
-            }
-            cl.End();
-        }
-    }
+    public abstract void BeginDraw(InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta);
 
-    public CommandList EndDraw()
-        => cl;
+    public abstract ValueTask<CommandList> EndDraw();
 
     public GUIElementList GUIElements { get; } = new();
+
+    protected void Draw(InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta)
+    {
+        cl.Begin();
+        cl.SetFramebuffer(gd.SwapchainFramebuffer); // Prepare for ImGUI
+        using (Controller.Begin()) // Lock ImGUI from other GraphicsManagers
+        {
+            foreach (var element in GUIElements)
+                element.InternalSubmitUI(delta); // Submit UIs
+            Controller.Update((float)delta.TotalSeconds, snapshot);
+            Controller.Render(gd, cl); // Render
+        }
+        cl.End();
+    }
 
     public ImGuiManager(ImGuiController controller, CommandList cl, GraphicsManager owner)
     {
         Controller = controller;
         OwnerManager = owner;
         this.cl = cl;
+    }
+
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        IsActive = false;
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                Controller.Dispose();
+                cl.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~ImGuiManager()
+    {
+        IsActive = false;
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        IsActive = false;
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+internal class ForegroundImGuiManager : ImGuiManager
+{
+    public ForegroundImGuiManager(ImGuiController controller, CommandList cl, GraphicsManager owner) : base(controller, cl, owner) { }
+    private (InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta) dpar;
+
+    public override void BeginDraw(InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta)
+    {
+        dpar = (snapshot, gd, delta);
+    }
+
+    public override ValueTask<CommandList> EndDraw()
+    {
+        var (sn, gd, d) = dpar;
+        Draw(sn, gd, d);
+        sn.ReturnToPool();
+        return ValueTask.FromResult(cl);
+    }
+}
+
+internal class BackgroundThreadImGuiManager : ImGuiManager
+{
+    private readonly Thread drtr;
+    private readonly SemaphoreSlim sem;
+    private (InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta) dpar;
+
+    public BackgroundThreadImGuiManager(ImGuiController controller, CommandList cl, GraphicsManager owner) : base(controller, cl, owner) 
+    {
+        drtr = drtr = new Thread(DrawThreadMethod);
+        sem = new(0, 1);
+        drtr.Start();
+    }
+
+    public override void BeginDraw(InputSnapshot snapshot, GraphicsDevice gd, TimeSpan delta)
+    {
+        dpar = (snapshot, gd, delta);
+        sem.Release();
+    }
+
+    public override async ValueTask<CommandList> EndDraw()
+    {
+        if (!sem.Wait(50))
+            await sem.WaitAsync();
+        return cl;
+    }
+
+    private void DrawThreadMethod()
+    {
+        while (IsActive)
+        {
+            try
+            {
+                sem.Wait();
+                try
+                {
+                    var (sn, gd, d) = dpar;
+                    Draw(sn, gd, d);
+                    sn.ReturnToPool();
+                }
+                finally
+                {
+                    sem.Release();
+                }
+            }
+            catch 
+            {
+#if DEBUG
+                throw;
+#endif
+            }
+        }
     }
 }
