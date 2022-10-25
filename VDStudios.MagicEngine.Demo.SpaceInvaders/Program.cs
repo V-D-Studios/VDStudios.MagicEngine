deb61d5e630b04753923ef7cd7f6b7f5881795d5
Diego García <dagarciam1014@gmail.com>
2022-10-25 16:55:59 -0400

    Created SpaceInvaders demo game project

---
+++ VDStudios.MagicEngine.Demo.SpaceInvaders/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VDStudios.MagicEngine.Demo.SpaceInvaders;
+
+public static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await Game.NewGame<SDLGame>().StartGame<MenuScene>();
+    }
+}
+
+public class MenuScene : Scene
+{
+}
