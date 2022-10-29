f159b9495983857d76e4c815d3ab45835f200c21
Diego García <dagarciam1014@gmail.com>
2022-10-29 19:11:17 -0400

    Update HighScoreScreenScene.cs

--- VDStudios.MagicEngine.Demo.SpaceInvaders/Scenes/HighScoreScreenScene.cs
+++ VDStudios.MagicEngine.Demo.SpaceInvaders/Scenes/HighScoreScreenScene.cs
@@ -1,10 +1,57 @@
-﻿namespace VDStudios.MagicEngine.Demo.SpaceInvaders.Scenes;
+﻿using ImGuiNET;
+using VDStudios.MagicEngine.Demo.SpaceInvaders.Services;
+
+namespace VDStudios.MagicEngine.Demo.SpaceInvaders.Scenes;
 
 public class HighScoreScreenScene : Scene
 {
+    private HighScoreImGuiWindow? Menu;
+
     protected override ValueTask Beginning()
     {
-        Game.SetScene(Program.MainMenuScene);
+        Menu = new(Services.GetService<HighScoreList>());
+        Game.MainGraphicsManager.AddElement(Menu);
+        Menu.MainWindowOpen = true;
         return base.Beginning();
+    }
+
+    protected override ValueTask Ending()
+    {
+        Menu?.Dispose();
+        return base.Ending();
+    }
+}
+
+public class HighScoreImGuiWindow : ImGuiElement
+{
+    public bool MainWindowOpen;
+    private readonly HighScoreList HighScores;
+    private readonly List<string> Scores;
+
+    public HighScoreImGuiWindow(HighScoreList highsc)
+    {
+        ArgumentNullException.ThrowIfNull(highsc);
+        HighScores = highsc;
+        Scores = new(highsc.Count);
+        highsc.CollectionChanged += Highsc_CollectionChanged;
+        foreach (var sc in highsc)
+            Scores.Add($"{sc.Score} @ {sc.TopLevel}; by {sc.Name}");
+    }
+
+    private void Highsc_CollectionChanged(HighScoreList sender, Utilities.Collections.CollectionChangedEventArgs<HighScore> eventArgs)
+    {
+        Scores.Clear();
+        foreach (var sc in sender)
+            Scores.Add($"{sc.Score} @ {sc.TopLevel}; by {sc.Name}");
+    }
+
+    protected override void SubmitUI(TimeSpan delta, IReadOnlyCollection<ImGuiElement> subElements)
+    {
+        ImGui.Begin("High Scores", ref MainWindowOpen, ImGuiWindowFlags.DockNodeHost);
+        foreach (var sc in Scores)
+            ImGui.BulletText(sc);
+        if (ImGui.Button("Clear"))
+            HighScores.Clear();
+        ImGui.End();
     }
 }
