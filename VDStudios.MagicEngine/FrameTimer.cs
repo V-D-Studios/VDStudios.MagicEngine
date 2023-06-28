dae209b81c55e1b1c75fda4f3f5ccf3c82bbef6b
Diego Garcia <dagarciam1014@gmail.com>
2023-06-28 00:05:10 -0400

    Create FrameTimer.cs

---
+++ VDStudios.MagicEngine/FrameTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// Represents a timer that can be used to time events using a <see cref="Game"/>'s frames as reference for time
+/// </summary>
+public readonly record struct FrameTimer(Game Game, uint Lapse)
+{
+    /// <summary>
+    /// The amount of frames to wait before the timer is triggered
+    /// </summary>
+    /// <remarks>
+    /// The timer will only be considered triggered if checked in the exact it is triggered, thus it needs to be checked every frame
+    /// </remarks>
+    public uint Lapse { get; } = Lapse + (uint)(Game.FrameCount % Lapse);
+
+    /// <summary>
+    /// Whether or not this <see cref="FrameTimer"/> is triggered this frame
+    /// </summary>
+    /// <remarks>
+    /// The timer will only be considered triggered if checked in the exact it is triggered, thus it needs to be checked every frame
+    /// </remarks>
+    public bool IsTriggered => Game.FrameCount % Lapse == 0;
+}
