7b74e2d003b3f71436e5ec32065651abb2fada24
Diego Garcia <dagarciam1014@gmail.com>
2023-06-29 20:10:18 -0400

    Started using the Window's View Matrix

--- VDStudios.MagicEngine/RenderTargets/PassthroughCamera2D.cs
+++ VDStudios.MagicEngine/RenderTargets/PassthroughCamera2D.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Veldrid;
@@ -34,6 +35,7 @@ public class PassthroughCamera2D : IRenderTarget
     {
         targetBuffer = device.SwapchainFramebuffer;
         targetParameters = drawParameters;
+        targetParameters.Transformation = new DrawTransformation(Owner.WindowView, Matrix4x4.Identity);
     }
 
     /// <inheritdoc/>
