2b2b7613b5e53fb7d17e7ca9688cf5b5df55b628
Diego Garcia <dagarciam1014@gmail.com>
2023-07-02 18:35:15 -0400

    Create UPoint.cs

---
+++ VDStudios.MagicEngine/UPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace VDStudios.MagicEngine;
+
+/// <summary>
+/// A point represented by two unsigned integers
+/// </summary>
+/// <param name="X">The X component of the point</param>
+/// <param name="Y">The Y component of the point</param>
+public readonly record struct UPoint(uint X, uint Y);
