781e7f938cc7ff98eb964d53147d293696f758d0
Diego Garcia <dagarciam1014@gmail.com>
2023-08-24 14:32:07 -0400

    Refactored and pulled TextureOperation to SDL library

---
+++ VDStudios.MagicEngine.Graphics.SDL/DrawOperations/TextureOperation.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using SDL2.NET;
+using SDL2.NET.SDLImage;
+using VDStudios.MagicEngine.Graphics;
+using VDStudios.MagicEngine.Graphics.SDL;
+using VDStudios.MagicEngine.SDL.Demo;
+
+namespace VDStudios.MagicEngine.Graphics.SDL.DrawOperations;
+
+/// <summary>
+/// A <see cref="DrawOperation{TGraphicsContext}"/> that renders a single texture
+/// </summary>
+public class TextureOperation : DrawOperation<SDLGraphicsContext>
+{
+    /// <summary>
+    /// Instances a new object of class <see cref="TextureOperation"/>
+    /// </summary>
+    /// <param name="game">The <see cref="Game"/> this <see cref="DrawOperation{TGraphicsContext}"/> belongs to</param>
+    /// <param name="textureFactory">A delegate that, when invoked, will produce the <see cref="Texture"/> to be rendered</param>
+    /// <param name="view">The first value for <see cref="View"/>. If null, it will be set to the <see cref="Texture"/>'s size at position 0,0 upon loading the <see cref="Texture"/></param>
+    public TextureOperation(Game game, Func<SDLGraphicsContext, Texture> textureFactory, Rectangle? view = null) : base(game)
+    {
+        TextureFactory = textureFactory ?? throw new ArgumentNullException(nameof(textureFactory));
+        View = view ?? View;
+    }
+
+    private Texture? texture;
+    private readonly Func<SDLGraphicsContext, Texture> TextureFactory;
+
+    /// <summary>
+    /// Represents the current view of the <see cref="Texture"/>, that is, the portion of the <see cref="Texture"/> that will be rendered
+    /// </summary>
+    public Rectangle View { get; set; }
+
+    /// <inheritdoc/>
+    protected override ValueTask CreateResources() => ValueTask.CompletedTask;
+
+    /// <inheritdoc/>
+    protected override void CreateGPUResources(SDLGraphicsContext context)
+    {
+        texture = TextureFactory(context);
+        if (View == default)
+            View = new Rectangle(texture.Size, default);
+
+        ColorTransformationChanged += PlayerRenderer_ColorTransformationChanged;
+    }
+
+    private void PlayerRenderer_ColorTransformationChanged(DrawOperation<SDLGraphicsContext> drawOperation, TimeSpan timestamp)
+    {
+        if (texture is not null)
+            this.ApplyColor(texture);
+    }
+
+    /// <inheritdoc/>
+    protected override void Draw(TimeSpan delta, SDLGraphicsContext context, RenderTarget<SDLGraphicsContext> target)
+    {
+        Debug.Assert(texture is not null);
+
+        Transform(scale: new Vector3(4, 4, 1));
+
+        var dest = this.CreateDestinationRectangle(View.Size.ToVector2(), target.Transformation).ToRectangle();
+        texture.Render(View, dest);
+    }
+
+    /// <inheritdoc/>
+    protected override void UpdateGPUState(SDLGraphicsContext context)
+    {
+    }
+}
--- VDStudios.MagicEngine.SDL.Demo/PlayerNode.cs
+++ VDStudios.MagicEngine.SDL.Demo/PlayerNode.cs
@@ -1,18 +1,20 @@
 ﻿using System.Diagnostics;
+using SDL2.NET.SDLImage;
 using VDStudios.MagicEngine.DemoResources;
 using VDStudios.MagicEngine.Graphics;
 using VDStudios.MagicEngine.Graphics.SDL;
+using VDStudios.MagicEngine.Graphics.SDL.DrawOperations;
 
 namespace VDStudios.MagicEngine.SDL.Demo;
 
 public class PlayerNode : Node
 {
-    private readonly CharacterAnimationContainer Animations;
-    private PlayerRenderer? Renderer;
+    private readonly CharacterAnimationContainer AnimationContainer;
+    private TextureOperation? SpriteOperation;
 
     public PlayerNode(Game game) : base(game)
     {
-        Animations = new(8,
+        AnimationContainer = new(8,
             Helper.GetRectangles(AnimationDefinitions.Robin.Player.Idle.Frames), true,
             Helper.GetRectangles(AnimationDefinitions.Robin.Player.Active.Frames), true,
             Helper.GetRectangles(AnimationDefinitions.Robin.Player.Up.Frames), true,
@@ -25,16 +27,16 @@ public class PlayerNode : Node
             Helper.GetRectangles(AnimationDefinitions.Robin.Player.DownLeft.Frames), true
         );
 
-        foreach (var (_, a) in Animations)
+        foreach (var (_, a) in AnimationContainer)
             a.StartHang = a.EndHang = TimeSpan.FromSeconds(1d / 2);
     }
 
     protected override ValueTask<bool> Updating(TimeSpan delta)
     {
-        Debug.Assert(Renderer is not null, "PlayerNode.Renderer is unexpectedly null at the time of rendering");
+        Debug.Assert(SpriteOperation is not null, "PlayerNode.SpriteOperation is unexpectedly null at the time of updating");
 
-        if (Animations.CurrentAnimation.Update())
-            Renderer.View = Animations.CurrentAnimation.CurrentElement;
+        if (AnimationContainer.CurrentAnimation.Update())
+            SpriteOperation.View = AnimationContainer.CurrentAnimation.CurrentElement;
 
         return base.Updating(delta);
     }
@@ -43,8 +45,13 @@ public class PlayerNode : Node
     {
         if (scene.GetDrawOperationManager<SDLGraphicsContext>(out var dopm))
         {
-            Renderer = new PlayerRenderer(Game);
-            await dopm.AddDrawOperation(Renderer);
+            SpriteOperation = new TextureOperation(Game, c =>
+            {
+                using var stream = new MemoryStream(Animations.Robin);
+                return Image.LoadTexture(c.Renderer, stream);
+            }, AnimationContainer.CurrentAnimation.CurrentElement);
+
+            await dopm.AddDrawOperation(SpriteOperation);
         }
         else
             Debug.Fail("The attached scene did not have a DrawOperationManager for SDLGraphicsContext");
