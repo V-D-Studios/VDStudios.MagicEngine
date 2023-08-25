053269cbc61e412351f651c201c1a6926cf6486d
Diego Garcia <dagarciam1014@gmail.com>
2023-08-24 20:47:18 -0400

    Create DemoScene.cs

---
+++ VDStudios.MagicEngine.SDL.Demo/DemoScene.cs
@@ -0,0 +1,21 @@
+﻿using VDStudios.MagicEngine.Graphics.SDL;
+using VDStudios.MagicEngine.Graphics.SDL.RenderTargets;
+
+namespace VDStudios.MagicEngine.SDL.Demo;
+
+public abstract class DemoScene : Scene
+{
+    private SDLCamera2D? _cam;
+    public SDLCamera2D Camera => _cam ?? throw new InvalidOperationException("Cannot access this Scene's Camera before the Scene is begun");
+
+    protected override ValueTask Beginning()
+    {
+        var manager = ((SDLGraphicsManager)Game.MainGraphicsManager);
+        manager.RenderTargets.Add(_cam = new SDLCamera2D(manager));
+        return ValueTask.CompletedTask;
+    }
+
+    protected DemoScene(Game game) : base(game)
+    {
+    }
+}
