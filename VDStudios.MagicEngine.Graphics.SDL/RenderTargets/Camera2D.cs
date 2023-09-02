72de7b8dd15d185b9b68f373c32157d0edbca697
Diego Garcia <dagarciam1014@gmail.com>
2023-09-02 01:18:54 -0400

    Updated SDLCamera2D with TransformationState

--- VDStudios.MagicEngine.Graphics.SDL/RenderTargets/Camera2D.cs
+++ VDStudios.MagicEngine.Graphics.SDL/RenderTargets/Camera2D.cs
@@ -13,8 +13,12 @@ namespace VDStudios.MagicEngine.Graphics.SDL.RenderTargets;
 /// </summary>
 public class SDLCamera2D : SDLRenderTarget
 {
-    private SDLCamera2D_State goal = new();
     private Matrix4x4 current = Matrix4x4.Identity;
+
+    /// <summary>
+    /// The current transformation goal for this <see cref="SDLCamera2D"/>
+    /// </summary>
+    public TransformationState Goal { get; } = new();
 
     /// <summary>
     /// The current Transformation Matrix that represents this <see cref="SDLCamera2D"/>'s view
@@ -49,7 +53,7 @@ public class SDLCamera2D : SDLRenderTarget
     /// <param name="rotY">The rotation along the y axis in worldspace for this operation</param>
     /// <param name="rotZ">The rotation along the z axis in worldspace for this operation</param>
     public void Move(Vector3? translation = null, Vector3? scale = null, Vector4? rotX = null, Vector4? rotY = null, Vector4? rotZ = null)
-        => goal.Transform(translation, scale, rotX, rotY, rotZ);
+        => Goal.Transform(translation, scale, rotX, rotY, rotZ);
 
     /// <summary>
     /// Forces the camera into the respective position
@@ -65,18 +69,15 @@ public class SDLCamera2D : SDLRenderTarget
     public void Set(Vector3? translation = null, Vector3? scale = null, Vector4? rotX = null, Vector4? rotY = null, Vector4? rotZ = null)
     {
         Move(translation, scale, rotX, rotY, rotZ);
-        current = goal.Transformation;
+        current = Goal.VertexTransformation;
     }
 
     /// <inheritdoc/>
     public override void BeginFrame(TimeSpan delta, SDLGraphicsContext context)
     {
         if (Target is IWorldMobile2D target)
-        {
-
-            goal.Transform(translation: new((context.Window.Size.ToVector2() / 2) - target.Position, 0), new Vector3(1, 1, 1));
-        }
-        current = Interpolator.Interpolate(current, goal.Transformation, (float)delta.TotalMilliseconds);
+            Goal.Transform(translation: new((context.Window.Size.ToVector2() / 4) - target.Position / 2, 0), scale: new Vector3(1, 1, 1));
+        current = Interpolator.Interpolate(current, Goal.VertexTransformation, (float)delta.TotalMilliseconds);
         Transformation = new DrawTransformation(current, Manager.WindowView);
     }
 
@@ -88,54 +89,4 @@ public class SDLCamera2D : SDLRenderTarget
 
     /// <inheritdoc/>
     public override void EndFrame(SDLGraphicsContext context) { }
-
-    private struct SDLCamera2D_State
-    {
-        public Matrix4x4 Transformation
-        {
-            get
-            {
-                if (vertrans is not Matrix4x4 t)
-                {
-                    var translation = Translation;
-                    var scl = Scale;
-                    var (cpxx, cpxy, cpxz, rotx) = RotationX;
-                    var (cpyx, cpyy, cpyz, roty) = RotationY;
-                    var (cpzx, cpzy, cpzz, rotz) = RotationZ;
-                    vertrans = t =
-                        Matrix4x4.CreateTranslation(translation) *
-                        Matrix4x4.CreateScale(scl) *
-                        Matrix4x4.CreateRotationX(rotx, new(cpxx, cpxy, cpxz)) *
-                        Matrix4x4.CreateRotationY(roty, new(cpyx, cpyy, cpyz)) *
-                        Matrix4x4.CreateRotationZ(rotz, new(cpzx, cpzy, cpzz));
-                }
-                return t;
-            }
-        }
-        private Matrix4x4? vertrans = Matrix4x4.Identity;
-
-        public Vector3 Translation { get; private set; }
-
-        public Vector3 Scale { get; private set; } = Vector3.One;
-
-        public Vector4 RotationX { get; private set; }
-
-        public Vector4 RotationY { get; private set; }
-
-        public Vector4 RotationZ { get; private set; }
-
-        public SDLCamera2D_State()
-        {
-        }
-
-        public void Transform(Vector3? translation = null, Vector3? scale = null, Vector4? rotX = null, Vector4? rotY = null, Vector4? rotZ = null)
-        {
-            Translation = translation ?? Translation;
-            Scale = scale ?? Scale;
-            RotationX = rotX ?? RotationX;
-            RotationY = rotY ?? RotationY;
-            RotationZ = rotZ ?? RotationZ;
-            vertrans = null;
-        }
-    }
 }
