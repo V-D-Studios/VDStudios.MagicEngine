d16870c05ce45f0ebdd96b661b2e19ff1259445c
Diego Garcia <dagarciam1014@gmail.com>
2023-09-02 00:03:53 -0400

    Separated TransformationState from DrawOperation

--- VDStudios.MagicEngine/Graphics/DrawOperation.cs
+++ VDStudios.MagicEngine/Graphics/DrawOperation.cs
@@ -12,64 +12,12 @@ namespace VDStudios.MagicEngine.Graphics;
 public abstract class DrawOperation<TGraphicsContext> : GraphicsObject<TGraphicsContext>, IDisposable
     where TGraphicsContext : GraphicsContext<TGraphicsContext>
 {
-    private readonly SemaphoreSlim sync = new(1, 1);
-
-    /// <summary>
-    /// Instances a new object of type <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    public DrawOperation(Game game) : base(game, "Drawing")
-    {
-    }
-
     #region Transformation
 
     /// <summary>
-    /// The transformation matrix that represents the current scaling properties in this <see cref="DrawOperation{TGraphicsContext}"/>
+    /// This <see cref="DrawOperation{TGraphicsContext}"/>'s current transformation state
     /// </summary>
-    /// <remarks>
-    /// <see cref="VertexTransformation"/> already includes this transformation and should not be mixed externally
-    /// </remarks>
-    public Matrix4x4 ScaleTransformation
-    {
-        get
-        {
-            if (scaletrans is not Matrix4x4 t)
-                scaletrans = t = Matrix4x4.CreateScale(Scale);
-            ScaleTransformationChanged?.Invoke(this, Game.TotalTime);
-            return t;
-        }
-    }
-    private Matrix4x4? scaletrans = Matrix4x4.Identity;
-
-    /// <summary>
-    /// The transformation matrix that represents the current transformation properties in this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    /// <remarks>
-    /// This transformation can be used to represent the current world properties of the drawing operation, for example, it's position and rotation in relation to the world itself
-    /// </remarks>
-    public Matrix4x4 VertexTransformation
-    {
-        get
-        {
-            if (vertrans is not Matrix4x4 t)
-            {
-                var translation = Translation;
-                var scl = Scale;
-                var (cpxx, cpxy, cpxz, rotx) = RotationX;
-                var (cpyx, cpyy, cpyz, roty) = RotationY;
-                var (cpzx, cpzy, cpzz, rotz) = RotationZ;
-                vertrans = t =
-                    Matrix4x4.CreateTranslation(translation) *
-                    Matrix4x4.CreateScale(scl) *
-                    Matrix4x4.CreateRotationX(rotx, new(cpxx, cpxy, cpxz)) *
-                    Matrix4x4.CreateRotationY(roty, new(cpyx, cpyy, cpyz)) *
-                    Matrix4x4.CreateRotationZ(rotz, new(cpzx, cpzy, cpzz));
-                VertexTransformationChanged?.Invoke(this, Game.TotalTime);
-            }
-            return t;
-        }
-    }
-    private Matrix4x4? vertrans = Matrix4x4.Identity;
+    public TransformationState TransformationState { get; } = new();
 
     /// <summary>
     /// The transformation matrix that represents the current transformation properties in this <see cref="DrawOperation{TGraphicsContext}"/>
@@ -92,81 +40,44 @@ public abstract class DrawOperation<TGraphicsContext> : GraphicsObject<TGraphics
     public event DrawOperationEvent<TGraphicsContext>? ColorTransformationChanged;
 
     /// <summary>
-    /// Fired when <see cref="VertexTransformation"/> changes
+    /// Fired when <see cref="TransformationState"/> <see cref="TransformationState.VertexTransformation"/> changes
     /// </summary>
+    /// <remarks>
+    /// Unlike <see cref="TransformationState.VertexTransformation"/>, this event belongs specifically to <see cref="DrawOperation{TGraphicsContext}"/>
+    /// </remarks>
     public event DrawOperationEvent<TGraphicsContext>? VertexTransformationChanged;
 
     /// <summary>
-    /// Fired when <see cref="ScaleTransformation"/> changes
+    /// Fired when <see cref="TransformationState"/> <see cref="TransformationState.ScaleTransformation"/> changes
     /// </summary>
+    /// <remarks>
+    /// Unlike <see cref="TransformationState.ScaleTransformation"/>, this event belongs specifically to <see cref="DrawOperation{TGraphicsContext}"/>
+    /// </remarks>
     public event DrawOperationEvent<TGraphicsContext>? ScaleTransformationChanged;
-
-    /// <summary>
-    /// Adjusts the transformation parameters and calculates the appropriate transformation matrix for this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    /// <remarks>
-    /// Parameters that are not specified (i.e. left as <c>null</c>) will default to the current transformation setting in this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </remarks>
-    /// <param name="translation">The translation in worldspace for this operation</param>
-    /// <param name="scale">The scale in worldspace for this operation</param>
-    /// <param name="rotX">The rotation along the x axis in worldspace for this operation</param>
-    /// <param name="rotY">The rotation along the y axis in worldspace for this operation</param>
-    /// <param name="rotZ">The rotation along the z axis in worldspace for this operation</param>
-    public void Transform(Vector3? translation = null, Vector3? scale = null, Vector4? rotX = null, Vector4? rotY = null, Vector4? rotZ = null)
-    {
-        if (scale is not null)
-        {
-            Scale = scale.Value;
-            scaletrans = null;
-        }
-        Translation = translation ?? Translation;
-        RotationX = rotX ?? RotationX;
-        RotationY = rotY ?? RotationY;
-        RotationZ = rotZ ?? RotationZ;
-        NotifyPendingGPUUpdate();
-        vertrans = null;
-    }
-
-    /// <summary>
-    /// Describes the current translation setting of this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    public Vector3 Translation { get; private set; }
-
-    /// <summary>
-    /// Describes the current scale setting of this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    public Vector3 Scale { get; private set; } = Vector3.One;
-
-    /// <summary>
-    /// Describes the current rotation setting along the x axis of this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    /// <remarks>
-    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
-    /// </remarks>
-    public Vector4 RotationX { get; private set; }
-
-    /// <summary>
-    /// Describes the current rotation setting along the y axis of this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    /// <remarks>
-    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
-    /// </remarks>
-    public Vector4 RotationY { get; private set; }
-
-    /// <summary>
-    /// Describes the current rotation setting along the z axis of this <see cref="DrawOperation{TGraphicsContext}"/>
-    /// </summary>
-    /// <remarks>
-    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
-    /// </remarks>
-    public Vector4 RotationZ { get; private set; }
 
     #endregion
 
+    #region Construction
+
+    private readonly SemaphoreSlim sync = new(1, 1);
+
     /// <summary>
-    /// Represents this <see cref="DrawOperation{TGraphicsContext}"/>'s preferred priority. May or may not be honored depending on the <see cref="DrawOperationManager{TGraphicsContext}"/>
+    /// Instances a new object of type <see cref="DrawOperation{TGraphicsContext}"/>
     /// </summary>
-    public float PreferredPriority { get; set; }
+    public DrawOperation(Game game) : base(game, "Drawing")
+    {
+        TransformationState.ScaleTransformationChanged += t =>
+        {
+            NotifyPendingGPUUpdate();
+            ScaleTransformationChanged?.Invoke(this, Game.TotalTime);
+        };
+
+        TransformationState.VertexTransformationChanged += t =>
+        {
+            NotifyPendingGPUUpdate();
+            VertexTransformationChanged?.Invoke(this, Game.TotalTime);
+        };
+    }
 
     /// <summary>
     /// The owner <see cref="DrawOperationManager{TGraphicsContext}"/> of this <see cref="DrawOperation{TGraphicsContext}"/>
@@ -185,6 +96,8 @@ public abstract class DrawOperation<TGraphicsContext> : GraphicsObject<TGraphics
             _owner = owner;
         }
     }
+
+    #endregion
 
     #region Registration
 
@@ -220,6 +133,11 @@ public abstract class DrawOperation<TGraphicsContext> : GraphicsObject<TGraphics
     #endregion
 
     #region Drawing
+
+    /// <summary>
+    /// Represents this <see cref="DrawOperation{TGraphicsContext}"/>'s preferred priority. May or may not be honored depending on the <see cref="DrawOperationManager{TGraphicsContext}"/>
+    /// </summary>
+    public float PreferredPriority { get; set; }
 
     #region Internal
 
---
+++ VDStudios.MagicEngine/Graphics/TransformationState.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Transactions;
+
+namespace VDStudios.MagicEngine.Graphics;
+
+/// <summary>
+/// Represents the current state of a transformation <see cref="Matrix4x4"/> given a set of different, singular parameters
+/// </summary>
+public sealed class TransformationState
+{
+    /// <summary>
+    /// The transformation matrix that represents the current scaling properties in this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// <see cref="VertexTransformation"/> already includes this transformation and should not be mixed externally
+    /// </remarks>
+    public Matrix4x4 ScaleTransformation
+    {
+        get
+        {
+            if (scaletrans is not Matrix4x4 t)
+                scaletrans = t = Matrix4x4.CreateScale(Scale);
+            ScaleTransformationChanged?.Invoke(this);
+            return t;
+        }
+    }
+    private Matrix4x4? scaletrans = Matrix4x4.Identity;
+
+    /// <summary>
+    /// The transformation matrix that represents the current transformation properties in this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// This transformation can be used to represent the current world properties of the drawing operation, for example, it's position and rotation in relation to the world itself
+    /// </remarks>
+    public Matrix4x4 VertexTransformation
+    {
+        get
+        {
+            if (vertrans is not Matrix4x4 t)
+            {
+                var translation = Translation;
+                var scl = Scale;
+                var (cpxx, cpxy, cpxz, rotx) = RotationX;
+                var (cpyx, cpyy, cpyz, roty) = RotationY;
+                var (cpzx, cpzy, cpzz, rotz) = RotationZ;
+                vertrans = t =
+                    Matrix4x4.CreateTranslation(translation) *
+                    Matrix4x4.CreateScale(scl) *
+                    Matrix4x4.CreateRotationX(rotx, new(cpxx, cpxy, cpxz)) *
+                    Matrix4x4.CreateRotationY(roty, new(cpyx, cpyy, cpyz)) *
+                    Matrix4x4.CreateRotationZ(rotz, new(cpzx, cpzy, cpzz));
+                VertexTransformationChanged?.Invoke(this);
+            }
+            return t;
+        }
+    }
+    private Matrix4x4? vertrans = Matrix4x4.Identity;
+    
+    /// <summary>
+    /// Fired when <see cref="VertexTransformation"/> changes
+    /// </summary>
+    public event GeneralGameEvent<TransformationState>? VertexTransformationChanged;
+
+    /// <summary>
+    /// Fired when <see cref="ScaleTransformation"/> changes
+    /// </summary>
+    public event GeneralGameEvent<TransformationState>? ScaleTransformationChanged;
+
+    /// <summary>
+    /// Adjusts the transformation parameters and calculates the appropriate transformation matrix for this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// Parameters that are not specified (i.e. left as <c>null</c>) will default to the current transformation setting in this <see cref="TransformationState"/>
+    /// </remarks>
+    /// <param name="translation">The translation in worldspace for this operation</param>
+    /// <param name="scale">The scale in worldspace for this operation</param>
+    /// <param name="rotX">The rotation along the x axis in worldspace for this operation</param>
+    /// <param name="rotY">The rotation along the y axis in worldspace for this operation</param>
+    /// <param name="rotZ">The rotation along the z axis in worldspace for this operation</param>
+    public void Transform(Vector3? translation = null, Vector3? scale = null, Vector4? rotX = null, Vector4? rotY = null, Vector4? rotZ = null)
+    {
+        if (scale is not null)
+        {
+            Scale = scale.Value;
+            scaletrans = null;
+        }
+        Translation = translation ?? Translation;
+        RotationX = rotX ?? RotationX;
+        RotationY = rotY ?? RotationY;
+        RotationZ = rotZ ?? RotationZ;
+        vertrans = null;
+    }
+
+    /// <summary>
+    /// Describes the current translation setting of this <see cref="TransformationState"/>
+    /// </summary>
+    public Vector3 Translation { get; private set; }
+
+    /// <summary>
+    /// Describes the current scale setting of this <see cref="TransformationState"/>
+    /// </summary>
+    public Vector3 Scale { get; private set; } = Vector3.One;
+
+    /// <summary>
+    /// Describes the current rotation setting along the x axis of this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
+    /// </remarks>
+    public Vector4 RotationX { get; private set; }
+
+    /// <summary>
+    /// Describes the current rotation setting along the y axis of this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
+    /// </remarks>
+    public Vector4 RotationY { get; private set; }
+
+    /// <summary>
+    /// Describes the current rotation setting along the z axis of this <see cref="TransformationState"/>
+    /// </summary>
+    /// <remarks>
+    /// Where <see cref="Vector4.X"/>, <see cref="Vector4.Y"/> and <see cref="Vector4.Z"/> are the center point, and <see cref="Vector4.W"/> is the actual rotation in <c>radians</c>
+    /// </remarks>
+    public Vector4 RotationZ { get; private set; }
+}
