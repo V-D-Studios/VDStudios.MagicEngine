e9c650e2b65866cbec6f8625b6c40d44e1940812
Diego Garcia <dagarciam1014@gmail.com>
2023-09-05 04:18:50 -0400

    Improved documentation

--- VDStudios.MagicEngine/World2D/IWorldObject2D.cs
+++ VDStudios.MagicEngine/World2D/IWorldObject2D.cs
@@ -10,11 +10,15 @@ public interface IWorldObject2D
     /// <summary>
     /// The position at which the object currently resides
     /// </summary>
-    public Vector2 Position { get; }
+    public Vector2 Position { get; set; }
 
     /// <summary>
     /// The size of the world object
     /// </summary>
-    public Vector2 Size { get; }
-    Vector2 Direction { get; }
+    public Vector2 Size { get; set; }
+
+    /// <summary>
+    /// The direction that this <see cref="IWorldObject2D"/> is facing
+    /// </summary>
+    public Vector2 Direction { get; set; }
 }
