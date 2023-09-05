801031014fc1fbb0bfa28bafeb5758f54dc85ade
Diego Garcia <dagarciam1014@gmail.com>
2023-09-05 18:32:04 -0400

    Improved GraphicsManagerFrameTimer

--- VDStudios.MagicEngine/GraphicsManagerFrameTimer.cs
+++ VDStudios.MagicEngine/GraphicsManagerFrameTimer.cs
@@ -10,6 +10,18 @@ public readonly record struct GraphicsManagerFrameTimer(GraphicsManager Graphics
     private readonly ulong Start = GraphicsManager.FrameCount;
 
     /// <summary>
+    /// Creates a new <see cref="GraphicsManagerFrameTimer"/> that is a copy of <paramref name="copy"/> with a new lapse
+    /// </summary>
+    /// <remarks>
+    /// Useful for changing the lapse of a <see cref="GraphicsManagerFrameTimer"/> without changing its starting point, so as to not lose unnecessary clocks
+    /// </remarks>
+    public GraphicsManagerFrameTimer(GraphicsManagerFrameTimer copy, uint lapse)
+        : this(copy.GraphicsManager, lapse)
+    {
+        Start = copy.Start;
+    }
+
+    /// <summary>
     /// The amount of frames to wait before the timer is triggered
     /// </summary>
     /// <remarks>
@@ -20,15 +32,18 @@ public readonly record struct GraphicsManagerFrameTimer(GraphicsManager Graphics
     /// <summary>
     /// The amount of times <see cref="Lapse"/> has elapsed
     /// </summary>
-    public uint Clocks => (uint)((GraphicsManager.FrameCount - Start) / Lapse);
+    /// <remarks>
+    /// If <see cref="Lapse"/> is <c>0</c>, this will always return <c>1</c>
+    /// </remarks>
+    public uint Clocks => Lapse == 0 ? 1 : (uint)((GraphicsManager.FrameCount - Start) / Lapse);
 
     /// <summary>
     /// Whether or not this timer is clocking at the current frame.
     /// </summary>
     /// <remarks>
-    /// This property is only reliable when called from within <see cref="GraphicsManager"/>'s rendering thread, that is, from within a <see cref="DrawOperation{TGraphicsContext}"/> belonging to this <see cref="GraphicsManager"/> or similar. If expected to work outside this thread, consider using <see cref="HasClocked"/> instead
+    /// This property is only reliable when called from within <see cref="GraphicsManager"/>'s rendering thread, that is, from within a <see cref="DrawOperation{TGraphicsContext}"/> belonging to this <see cref="GraphicsManager"/> or similar. If expected to work outside this thread, consider using <see cref="HasClocked"/> instead. Will always return <see langword="true"/> if <see cref="Lapse"/> is <c>0</c>
     /// </remarks>
-    public bool IsClocking => (GraphicsManager.FrameCount - Start) % Lapse == 0;
+    public bool IsClocking => Lapse == 0 || (GraphicsManager.FrameCount - Start) % Lapse == 0;
 
     /// <summary>
     /// Whether or not this <see cref="GraphicsManagerFrameTimer"/> has clocked at least once since it starting
