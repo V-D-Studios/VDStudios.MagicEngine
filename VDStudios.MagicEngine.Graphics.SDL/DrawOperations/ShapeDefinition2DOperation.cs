254b108e18962bd0452b094d75edd982404d591e
Diego Garcia <dagarciam1014@gmail.com>
2023-09-05 21:04:20 -0400

    Create ShapeDefinition2DOperation.cs

---
+++ VDStudios.MagicEngine.Graphics.SDL/DrawOperations/ShapeDefinition2DOperation.cs
@@ -0,0 +1,89 @@
+﻿using System.Numerics;
+using SDL2.NET;
+using VDStudios.MagicEngine.Geometry;
+using VDStudios.MagicEngine.SDL.Demo;
+
+namespace VDStudios.MagicEngine.Graphics.SDL.DrawOperations;
+
+/// <summary>
+/// An operation that renders a <see cref="ShapeDefinition2D"/>
+/// </summary>
+public class ShapeDefinition2DOperation : DrawOperation<SDLGraphicsContext>
+{
+    /// <summary>
+    /// Creates a new object of type <see cref="TextOperation"/>
+    /// </summary>
+    public ShapeDefinition2DOperation(ShapeDefinition2D shape, Game game) : base(game)
+    {
+        _shape = shape;
+    }
+
+    /// <summary>
+    /// The shape that will be Rendered
+    /// </summary>
+    public ShapeDefinition2D Shape
+    {
+        get => _shape;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if(_shape != value)
+            {
+                _shape = value;
+                lastver = _shape.Version;
+                NotifyPendingGPUUpdate();
+            }
+        }
+    }
+    private ShapeDefinition2D _shape;
+    private int lastver = 0;
+
+    /// <summary>
+    /// The color of the rendered shapes
+    /// </summary>
+    public RGBAColor Color { get; set; } = RgbaVector.DarkRed.ToRGBAColor();
+
+    /// <inheritdoc/>
+    protected override ValueTask CreateResourcesAsync()
+        => ValueTask.CompletedTask;
+
+    /// <inheritdoc/>
+    protected override void CreateGPUResources(SDLGraphicsContext context) { }
+
+    /// <inheritdoc/>
+    protected override void Draw(TimeSpan delta, SDLGraphicsContext context, RenderTarget<SDLGraphicsContext> target)
+    {
+        if (lastver != _shape.Version)
+        {
+            ForceGPUUpdate(context);
+            lastver = _shape.Version;
+        }
+
+        var sh = Shape;
+        Span<Vector2> vertices = stackalloc Vector2[sh.Count];
+        sh.AsSpan().CopyTo(vertices);
+
+        if (sh is RectangleDefinition rectangle)
+        {
+            context.Renderer.DrawRectangle(rectangle.ToFloatRectangle().ToRectangle(), Color);
+        }
+        else if (sh is ElipseDefinition elipse)
+        {
+            throw new NotSupportedException();
+        }
+        else if (sh is CircleDefinition circle)
+        {
+            throw new NotSupportedException();
+        }
+        else
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override void UpdateGPUState(SDLGraphicsContext context)
+    {
+        
+    }
+}
---
+++ VDStudios.MagicEngine/Geometry/RectangleDefinition.cs
@@ -0,0 +1,92 @@
+﻿using System.Numerics;
+
+namespace VDStudios.MagicEngine.Geometry;
+
+/// <summary>
+/// Represents the definition of a rectangle
+/// </summary>
+public class RectangleDefinition : ShapeDefinition2D
+{
+    /// <summary>
+    /// The Position of this Rectangle
+    /// </summary>
+    public Vector2 Position { get; }
+
+    /// <summary>
+    /// The size of this Rectangle
+    /// </summary>
+    public Vector2 Size { get; }
+
+    /// <summary>
+    /// Represents this <see cref="RectangleDefinition"/> as a <see cref="FloatRectangle"/>
+    /// </summary>
+    public FloatRectangle ToFloatRectangle()
+        => new(Position, Size);
+
+    private readonly Vector2[] vertices = new Vector2[4];
+
+    /// <summary>
+    /// Creates a new RectangleDefinition
+    /// </summary>
+    public RectangleDefinition(FloatRectangle rectangle) : this(rectangle.Position, rectangle.Size) { }
+
+    /// <summary>
+    /// Creates a new RectangleDefinition
+    /// </summary>
+    public RectangleDefinition(Vector2 position, Vector2 size) : base(true)
+    {
+        Position = position;
+        Size = size;
+
+        vertices = new Vector2[4]
+        {
+            Position,
+            new(Position.X, Position.Y + Size.Y),
+            Position + Size,
+            new(Position.X + Size.X, Position.Y)
+        };
+    }
+
+    /// <inheritdoc/>
+    public override bool ForceRegenerate()
+        => false;
+
+    /// <inheritdoc/>
+    public override ReadOnlySpan<Vector2> AsSpan(int start, int length)
+        => vertices.AsSpan(start, length);
+
+    /// <inheritdoc/>
+    public override ReadOnlySpan<Vector2> AsSpan(int start)
+        => vertices.AsSpan(start);
+
+    /// <inheritdoc/>
+    public override ReadOnlySpan<Vector2> AsSpan()
+        => vertices.AsSpan();
+
+    /// <inheritdoc/>
+    public override void CopyTo(Span<Vector2> destination)
+        => vertices.CopyTo(destination);
+
+    /// <inheritdoc/>
+    public override IEnumerator<Vector2> GetEnumerator()
+        => ((IEnumerable<Vector2>)vertices).GetEnumerator();
+
+    /// <inheritdoc/>
+    public override Vector2[] ToArray()
+    {
+        var a = new Vector2[vertices.Length];
+        vertices.CopyTo(a, 0);
+        return a;
+    }
+
+    /// <inheritdoc/>
+    public override bool TryCopyTo(Span<Vector2> destination)
+        => vertices.AsSpan().TryCopyTo(destination);
+
+    /// <inheritdoc/>
+    public override Vector2 this[int index] 
+        => vertices[index];
+
+    /// <inheritdoc/>
+    public override int Count => vertices.Length;
+}
