8af13d5feb8ecbafb8e74ff1a815507a3f4e87fc
Diego Garcia <dagarciam1014@gmail.com>
2023-09-06 05:45:30 -0400

    Update GameObjectId.cs

--- VDStudios.MagicEngine/GameObjectId.cs
+++ VDStudios.MagicEngine/GameObjectId.cs
@@ -1,4 +1,6 @@
 ﻿using System.Buffers.Text;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -10,8 +12,12 @@ namespace VDStudios.MagicEngine;
 /// Represents an unique Id given to every <see cref="GameObject"/> for varied purposes, primarily debugging and logging
 /// </summary>
 [StructLayout(LayoutKind.Explicit)]
-public readonly struct GameObjectId : IEquatable<GameObjectId>
+[DebuggerDisplay("Id {ToString()} - Raw {Raw}")]
+public readonly struct GameObjectId : IEquatable<GameObjectId>, ISpanParsable<GameObjectId>
 {
+    const string IdCharpool = "0A1B2C3D4E5H6G7U8J9KMPQRSTVWXYZF";
+    const int StringLength = 16;
+
     /// <summary>
     /// Creates a new <see cref="GameObjectId"/>
     /// </summary>
@@ -19,13 +25,16 @@ public readonly struct GameObjectId : IEquatable<GameObjectId>
     public static unsafe GameObjectId NewId() 
         => new(DateTime.Now);
 
+    internal GameObjectId(ulong raw)
+        => Raw = raw;
+
     private unsafe GameObjectId(DateTime dt)
     {
-        TimeStamp = (uint)(dt - DateTime.UnixEpoch).TotalSeconds;
+        TimeStamp = (uint)(dt - DateTime.UnixEpoch).TotalMilliseconds;
         RandomNumberGenerator.Fill(MemoryMarshal.Cast<uint, byte>(new Span<uint>(ref salt)));
     }
 
-    [field: FieldOffset(0)]
+    [FieldOffset(0)]
     internal readonly ulong Raw;
 
     /// <summary>
@@ -43,22 +52,26 @@ public readonly struct GameObjectId : IEquatable<GameObjectId>
     /// <inheritdoc/>
     public override string ToString()
     {
-        if (BitConverter.IsLittleEndian is false)
+        if (BitConverter.IsLittleEndian is false )
             throw new NotSupportedException("BigEndian is not supported. Figure out how it looks against LittleEndian and just flip the bytes in this method or smth. Library error, contact author.");
 
-        const string charpool = "0123456789ABCDEFGHJKLMNPQRSTWXYZ";
-        Span<char> chars = stackalloc char[15];
+        Span<char> chars = stackalloc char[StringLength];
         chars[0] = 'x';
         chars[1] = ':';
+
         int i = 2;
         var v = Raw;
         while (v > 0)
         {
-            chars[i++] = charpool[(int)(v % (ulong)charpool.Length)];
-            v /= (ulong)charpool.Length;
+            chars[i++] = IdCharpool[(int)(v % (ulong)IdCharpool.Length)];
+            v /= (ulong)IdCharpool.Length;
         }
 
-        for (; i < chars.Length; i++) chars[i] = 'ñ';
+        var i2 = StringLength - i + 2;
+        if (i2 == 2) return new string(chars);
+
+        chars[2..^i].CopyTo(chars[i2..]);
+        for (; i2 < i2 - i; i2++) chars[i2] = '0';
 
         return new string(chars);
     }
@@ -82,4 +95,74 @@ public readonly struct GameObjectId : IEquatable<GameObjectId>
     /// <inheritdoc/>
     public override int GetHashCode()
         => Raw.GetHashCode();
+
+    private static bool CheckFormat(ref ReadOnlySpan<char> s)
+    {
+        if (s.StartsWith("x:", StringComparison.OrdinalIgnoreCase))
+            s = s[2..];
+
+        if (s.Length is not StringLength - 2)
+            return false;
+
+        for (int i = 0; i < StringLength - 2; i++)
+            if (IdCharpool.Contains(s[i]) is false)
+                return false;
+
+        return true;
+    }
+
+    private static GameObjectId ParseId(ReadOnlySpan<char> s)
+    {
+        throw new NotImplementedException();
+        ulong x = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            var indx = (ulong)IdCharpool.IndexOf(s[i]);
+            x = (x + indx) * (ulong)IdCharpool.Length;
+        }
+        return new GameObjectId(x);
+    }
+
+    /// <inheritdoc/>
+    public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [MaybeNullWhen(false)] out GameObjectId result)
+    {
+        if (CheckFormat(ref s) is false)
+        {
+            result = default;
+            return false;
+        }
+
+        result = ParseId(s);
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public static GameObjectId Parse(ReadOnlySpan<char> s, IFormatProvider? provider = null) 
+        => CheckFormat(ref s) is false ? throw new FormatException("Argument s was in an incorrect format") : ParseId(s);
+
+    /// <summary>
+    /// Tries to parse a span of characters into a value.
+    /// </summary>
+    /// <param name="s">The span of characters to parse.</param>
+    /// <param name="result">When this method returns, contains the result of successfully parsing <paramref name="s"/>, or an undefined value on failure.</param>
+    /// <returns><see langword="true"/> if <paramref name="s"/> was successfully parsed; otherwise, <see langword="false"/>.</returns>
+    public static bool TryParse(ReadOnlySpan<char> s, [MaybeNullWhen(false)] out GameObjectId result)
+        => TryParse(s, null, out result);
+
+    /// <inheritdoc/>
+    public static GameObjectId Parse(string s, IFormatProvider? provider = null)
+        => Parse(s.AsSpan(), provider);
+
+    /// <inheritdoc/>
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out GameObjectId result)
+        => TryParse(s.AsSpan(), provider, out result);
+
+    /// <summary>
+    /// Tries to parse a span of characters into a value.
+    /// </summary>
+    /// <param name="s">The span of characters to parse.</param>
+    /// <param name="result">When this method returns, contains the result of successfully parsing <paramref name="s"/>, or an undefined value on failure.</param>
+    /// <returns><see langword="true"/> if <paramref name="s"/> was successfully parsed; otherwise, <see langword="false"/>.</returns>
+    public static bool TryParse([NotNullWhen(true)] string? s, [MaybeNullWhen(false)] out GameObjectId result)
+        => TryParse(s, null, out result);
 }
