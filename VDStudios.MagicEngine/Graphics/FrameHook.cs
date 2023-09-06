b9f1d989095598efce11f009cc0a09c7741fab87
Diego Garcia <dagarciam1014@gmail.com>
2023-09-06 15:35:01 -0400

    Update FrameHook.cs

--- VDStudios.MagicEngine/Graphics/FrameHook.cs
+++ VDStudios.MagicEngine/Graphics/FrameHook.cs
@@ -6,7 +6,7 @@
 /// <remarks>
 /// This object is an abstraction, and the actual data is handled and seen entirely on <see cref="GraphicsManager{TGraphicsContext}"/>'s implementation of this <see cref="FrameHook"/>
 /// </remarks>
-public abstract class FrameHook : IDisposable
+public abstract class FrameHook : DisposableGameObject
 {
     /// <summary>
     /// The <see cref="GraphicsManager{TGraphicsContext}"/> this <see cref="FrameHook"/> is watching
@@ -16,24 +16,8 @@ public abstract class FrameHook : IDisposable
     /// <summary>
     /// Creates a new object of type <see cref="FrameHook"/>
     /// </summary>
-    protected FrameHook(GraphicsManager owner)
+    protected FrameHook(GraphicsManager owner) : base(owner.Game, "Graphics & Input", "Video Recording")
     {
         Owner = owner ?? throw new ArgumentNullException(nameof(owner));
-    }
-
-    /// <summary>
-    /// Disposes of this <see cref="FrameHook"/>
-    /// </summary>
-    /// <remarks>
-    /// Disconnects this <see cref="FrameHook"/> from its owner <see cref="GraphicsManager{TGraphicsContext}"/>
-    /// </remarks>
-    /// <param name="disposing"></param>
-    protected abstract void Dispose(bool disposing);
-
-    /// <inheritdoc/>
-    public void Dispose()
-    {
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
     }
 }
