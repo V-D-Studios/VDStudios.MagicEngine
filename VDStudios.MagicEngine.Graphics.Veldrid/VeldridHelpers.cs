0d8e3854a6a339d13710220396f20b225d157e63
Diego Garcia <dagarciam1014@gmail.com>
2023-09-08 15:01:51 -0400

    Create VeldridHelpers.cs

---
+++ VDStudios.MagicEngine.Graphics.Veldrid/VeldridHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Veldrid;
+
+namespace VDStudios.MagicEngine.Graphics.Veldrid;
+
+/// <summary>
+/// Assorted helpers for Veldrid classes
+/// </summary>
+public static class VeldridHelpers
+{
+    /// <summary>
+    /// Converts a <see cref="RgbaVector"/> into a Veldrid <see cref="RgbaFloat"/>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe RgbaFloat ToRgbaFloat(this RgbaVector vector) 
+        => *(RgbaFloat*)(&vector);
+
+    /// <summary>
+    /// Converts a <see cref="RgbaVector"/> into a Veldrid <see cref="RgbaFloat"/>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe ref RgbaFloat ToRgbaFloatRef(this ref RgbaVector vector)
+        => ref Unsafe.AsRef<RgbaFloat>(Unsafe.AsPointer(ref vector));
+
+    /// <summary>
+    /// Converts a <see cref="RgbaFloat"/> into a Veldrid <see cref="RgbaVector"/>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe RgbaVector ToRgbaVector(this RgbaFloat vector)
+        => *(RgbaVector*)(&vector);
+
+    /// <summary>
+    /// Converts a <see cref="RgbaFloat"/> into a Veldrid <see cref="RgbaVector"/>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe ref RgbaVector ToRgbaVectorRef(this ref RgbaFloat vector)
+        => ref Unsafe.AsRef<RgbaVector>(Unsafe.AsPointer(ref vector));
+}
