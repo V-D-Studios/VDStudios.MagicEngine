528b436aaa0f7b9e55f83b2e4b432dc7e1207a88
Diego Garcia <dagarciam1014@gmail.com>
2023-09-10 16:56:59 -0400

    Rewrote DefaultShaders

--- VDStudios.MagicEngine.Graphics.Veldrid/Properties/DefaultShaders.cs
+++ VDStudios.MagicEngine.Graphics.Veldrid/Properties/DefaultShaders.cs
@@ -1,4 +1,6 @@
-﻿namespace VDStudios.MagicEngine.Graphics.Veldrid.Properties;
+﻿using VDStudios.MagicEngine.Graphics.Veldrid.DrawOperations;
+
+namespace VDStudios.MagicEngine.Graphics.Veldrid.Properties;
 
 /// <summary>
 /// This class presents access to MagicEngine's default shaders
@@ -52,63 +54,80 @@ void main() {
     #region ShapeRenderer
 
     /// <summary>
-    /// The default vertex shader for <see cref="ShapeRenderer{TVertex}"/>
+    /// The default vertex shader for <see cref="Shape2DRenderer"/>
     /// </summary>
-    public const string DefaultShapeRendererVertexShader = @"#version 450
+    public const string DefaultShape2DRendererVertexShader = """
+        #version 450
+        
+        layout(location = 0) in vec2 Position;
+        layout(location = 1) in vec4 Color;
+        layout(location = 0) out vec4 fsin_Color;
+        layout(set=0,binding=0) uniform FrameReport {
+            layout(offset = 0) mat4 projection;
+            layout(offset = 64) float delta;
+        };
+        layout(set=1,binding=0) uniform Parameters {
+            layout(offset = 0) mat4 view;
+        };
+        layout(set=2,binding=0) uniform DrawOp {
+            layout(offset = 0) mat4 transform;
+        }
+        
+        void main() {
+            fsin_Color = Color;
+            gl_Position = projection * view * transform * vec4(Position, 0.0, 1.0);
+        }
+        """;
 
-layout(location = 0) in vec2 Position;
-layout(location = 1) in vec4 Color;
-layout(location = 0) out vec4 fsin_Color;
-layout(set=0,binding=0) uniform Transform {
-    layout(offset = 0) mat4 opTrans;
-};
-layout(set=1,binding=0) uniform Parameters {
-    layout(offset = 0) mat4 view;
-    layout(offset = 64) mat4 projection;
-};
-
-void main() {
-    fsin_Color = Color;
-    gl_Position = projection * view * opTrans * vec4(Position, 0.0, 1.0);
-}";
+    internal static readonly byte[] DefaultShape2DRendererVertexShaderBytes
+        = Encoding.UTF8.GetBytes(DefaultShape2DRendererVertexShader);
 
     /// <summary>
-    /// The default fragment shader for <see cref="ShapeRenderer{TVertex}"/>
+    /// The default fragment shader for <see cref="Shape2DRenderer"/>
     /// </summary>
-    public const string DefaultShapeRendererFragmentShader = @"#version 450
+    public const string DefaultShape2DRendererFragmentShader = """
+        #version 450
+        
+        const int grayscaleFx = 1 << 0;
+        const int tintFx = 1 << 1;
+        const int overlayFx = 1 << 2;
+        const int opacityOverrideFx = 1 << 3;
+        const int opacityMultiplyFx = 1 << 4;
+        
+        layout(location = 0) out vec4 fsout_Color;
+        layout(location = 0) in vec4 fsin_Color;
+        
+        layout(set=0,binding=0) uniform FrameReport {
+            layout(offset = 64) float delta;
+        };
+        
+        layout(set=2,binding=0) uniform Transform {
+            layout(offset = 0) mat4 opTrans;
+            layout(offset = 64) vec4 tint;
+            layout(offset = 80) vec4 overlay;
+            layout(offset = 96) uint colorfx;
+            layout(offset = 100) float opacity;
+        } trans;
+        
+        vec4 toGrayscale(vec4 color)
+        {
+            float average = (color.r + color.g + color.b) / 3.0;
+            return vec4(average, average, average, color.a);
+        }
+        
+        void main() {
+            vec4 c = fsin_Color;
+            if ((trans.colorfx & grayscaleFx) != 0) { c = toGrayscale(c); }
+            if ((trans.colorfx & tintFx) != 0) { c = vec4(c.r * trans.tint.r, c.g * trans.tint.g, c.b * trans.tint.b, c.a); }
+            if ((trans.colorfx & overlayFx) != 0) { c *= trans.overlay; }
+            if ((trans.colorfx & opacityOverrideFx) != 0) { c.a = trans.opacity; }
+            else if ((trans.colorfx & opacityMultiplyFx) != 0) { c.a *= trans.opacity; }
+            fsout_Color = c;
+        }
+        """;
 
-const int grayscaleFx = 1 << 0;
-const int tintFx = 1 << 1;
-const int overlayFx = 1 << 2;
-const int opacityOverrideFx = 1 << 3;
-const int opacityMultiplyFx = 1 << 4;
-
-layout(location = 0) out vec4 fsout_Color;
-layout(location = 0) in vec4 fsin_Color;
-
-layout(set=1,binding=0) uniform Transform {
-    layout(offset = 0) mat4 opTrans;
-    layout(offset = 64) vec4 tint;
-    layout(offset = 80) vec4 overlay;
-    layout(offset = 96) uint colorfx;
-    layout(offset = 100) float opacity;
-} trans;
-
-vec4 toGrayscale(vec4 color)
-{
-    float average = (color.r + color.g + color.b) / 3.0;
-    return vec4(average, average, average, color.a);
-}
-
-void main() {
-    vec4 c = fsin_Color;
-    if ((trans.colorfx & grayscaleFx) != 0) { c = toGrayscale(c); }
-    if ((trans.colorfx & tintFx) != 0) { c = vec4(c.r * trans.tint.r, c.g * trans.tint.g, c.b * trans.tint.b, c.a); }
-    if ((trans.colorfx & overlayFx) != 0) { c *= trans.overlay; }
-    if ((trans.colorfx & opacityOverrideFx) != 0) { c.a = trans.opacity; }
-    else if ((trans.colorfx & opacityMultiplyFx) != 0) { c.a *= trans.opacity; }
-    fsout_Color = c;
-}";
+    internal static readonly byte[] DefaultShape2DRendererFragmentShaderBytes
+        = Encoding.UTF8.GetBytes(DefaultShape2DRendererFragmentShader);
 
     #endregion
 }
