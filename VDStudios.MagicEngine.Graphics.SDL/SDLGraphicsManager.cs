1dfe07ffa5377c00f8aeedd8047b75475822cbe7
Diego Garcia <dagarciam1014@gmail.com>
2023-09-11 00:24:10 -0400

    Split SDLGraphicsManager into SDL.Base

--- VDStudios.MagicEngine.Graphics.SDL/SDLGraphicsManager.cs
+++ VDStudios.MagicEngine.Graphics.SDL/SDLGraphicsManager.cs
@@ -17,7 +17,7 @@ namespace VDStudios.MagicEngine.Graphics.SDL;
 /// <summary>
 /// A <see cref="GraphicsManager{TGraphicsContext}"/> for SDL
 /// </summary>
-public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
+public class SDLGraphicsManager : MagicEngine.SDL.Base.SDLGraphicsManagerBase<SDLGraphicsContext>
 {
     /// <summary>
     /// The list of ImGUI Elements
@@ -63,12 +63,6 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
     private SDLGraphicsContext? context;
 
     /// <summary>
-    /// The SDL Window managed by this <see cref="SDLGraphicsManager"/>
-    /// </summary>
-    public Window Window { get => window ?? throw new InvalidOperationException("Cannot get the Window of an SDLGraphicsManager that has not been launched"); private set => window = value; }
-    private Window? window;
-
-    /// <summary>
     /// The SDL Renderer managed by this <see cref="SDLGraphicsManager"/>
     /// </summary>
     public Renderer Renderer { get => renderer ?? throw new InvalidOperationException("Cannot get the Renderer of an SDLGraphicsManager that has not been launched"); private set => renderer = value; }
@@ -83,7 +77,6 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
     public SDLGraphicsManager(Game game, WindowConfig? windowConfig = null) : base(game) 
     {
         WindowConfig = windowConfig ?? WindowConfig.Default;
-        WindowActionQueue = new ConcurrentQueue<Action<Window>>();
         imGuiController = new(this);
         WindowView = Matrix4x4.Identity;
     }
@@ -91,7 +84,6 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
     /// <inheritdoc/>
     protected override SDLGraphicsContext FetchGraphicsContext()
     {
-        Debug.Assert(window is not null, "Window is unexpectedly null");
         Debug.Assert(renderer is not null, "Renderer is unexpectedly null");
         lock (Sync)
             return context ??= new(this);
@@ -108,32 +100,12 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
     /// </summary>
     protected void DisposeSDLResources()
     {
-        if (Window is Window win)
-        {
-            Renderer ren = Renderer;
-            Debug.Assert(ren is not null, "Despite Window not being null, Renderer is unexpectedly null");
-
-            win.Closed -= Window_Closed;
-            win.SizeChanged -= Window_SizeChanged;
-            win.Shown -= Window_Shown;
-            win.FocusGained -= Window_FocusGained;
-            win.FocusTaken -= Window_FocusGained;
-            win.FocusLost -= Window_FocusLost;
-            win.Hidden -= Window_Hidden;
-            win.KeyPressed -= Window_KeyPressed;
-            win.KeyReleased -= Window_KeyReleased;
-            win.MouseButtonPressed -= Window_MouseButtonPressed;
-            win.MouseButtonReleased -= Window_MouseButtonReleased;
-            win.MouseMoved -= Window_MouseMoved;
-
+        if (renderer is not null)
             Renderer.Dispose();
-            win.Dispose();
-        }
 
-        window = null;
+        ReleaseWindow();
+
         renderer = null;
-
-        IsWindowAvailable = false;
         IsRunning = false;
     }
 
@@ -165,29 +137,7 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
 
     #region Window Thread
 
-    internal readonly SemaphoreSlim WindowThreadLock = new(1, 1);
     private Task? WindowThread;
-    private readonly ConcurrentQueue<Action<Window>> WindowActionQueue;
-
-    /// <summary>
-    /// Performs the desired action on <see cref="Window"/>, and returns immediately
-    /// </summary>
-    /// <remarks>
-    /// Failure to perform on <see cref="Window"/> from this method will result in an exception, as <see cref="SDL2.NET.Window"/> is NOT thread-safe and, in fact, thread-protected
-    /// </remarks>
-    /// <param name="action">The action to perform on <see cref="Window"/></param>
-    public void PerformOnWindow(Action<Window> action)
-    {
-        WindowActionQueue.Enqueue(action);
-    }
-
-    /// <summary>
-    /// This method is called automatically when <see cref="Window"/> changes its size
-    /// </summary>
-    /// <remarks>
-    /// This method is called after the frame is unlocked
-    /// </remarks>
-    protected virtual void WindowSizeChanged(TimeSpan timestamp, Size newSize) { }
 
     /// <inheritdoc/>
     protected override void BeforeRun()
@@ -195,32 +145,7 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
         Window.CreateWindowAndRenderer(Game.GameTitle, 800, 600, out var win, out var ren, configuration: WindowConfig);
         Window = win;
         Renderer = ren;
-
-        Window.Closed += Window_Closed;
-        Window.SizeChanged += Window_SizeChanged;
-        Window.Shown += Window_Shown;
-        Window.FocusGained += Window_FocusGained;
-        Window.FocusTaken += Window_FocusGained;
-        Window.FocusLost += Window_FocusLost;
-        Window.Hidden += Window_Hidden;
-        Window.KeyPressed += Window_KeyPressed;
-        Window.KeyReleased += Window_KeyReleased;
-        Window.MouseButtonPressed += Window_MouseButtonPressed;
-        Window.MouseButtonReleased += Window_MouseButtonReleased;
-        Window.MouseMoved += Window_MouseMoved;
-        Window.MouseWheelScrolled += Window_MouseWheelScrolled;
-
-        PerformOnWindow(w =>
-        {
-            Log?.Debug("Querying WindowFlags");
-            var flags = w.Flags;
-            IsWindowAvailable = flags.HasFlag(WindowFlags.Shown);
-            HasFocus = flags.HasFlag(WindowFlags.InputFocus);
-
-            Log?.Debug("Reading WindowSize");
-            var (ww, wh) = w.Size;
-            WindowSize = new IntVector2(ww, wh);
-        });
+        ConfigureWindow();
     }
 
     /// <inheritdoc/>
@@ -269,99 +194,6 @@ public class SDLGraphicsManager : GraphicsManager<SDLGraphicsContext>
         FrameLock.Wait();
         FrameLock.Release();
     }
-
-    private void Window_MouseWheelScrolled(Window sender, TimeSpan timestamp, uint mouseId, float verticalScroll, float horizontalScroll)
-    {
-        ReportMouseWheelEvent(mouseId, new Vector2(horizontalScroll, verticalScroll));
-    }
-
-    #region Window Events
-
-    private void Window_MouseMoved(Window sender, TimeSpan timestamp, Point delta, Point newPosition, uint mouseId, SDL2.NET.MouseButton pressed)
-    {
-        ReportMouseMoved(mouseId, delta.ToVector2(), newPosition.ToVector2(), (Input.MouseButton)pressed);
-    }
-
-    private void Window_MouseButtonReleased(Window sender, TimeSpan timestamp, uint mouseId, int clicks, SDL2.NET.MouseButton state)
-    {
-        ReportMouseButtonReleased(mouseId, clicks, (Input.MouseButton)state);
-    }
-
-    private void Window_MouseButtonPressed(Window sender, TimeSpan timestamp, uint mouseId, int clicks, SDL2.NET.MouseButton state)
-    {
-        ReportMouseButtonPressed(mouseId, clicks, (Input.MouseButton)state);
-    }
-
-    private void Window_KeyReleased(Window sender, TimeSpan timestamp, SDL2.NET.Scancode scancode, SDL2.NET.Keycode key, SDL2.NET.KeyModifier modifiers, bool isPressed, bool repeat, uint unicode)
-    {
-        ReportKeyReleased(0, (Input.Scancode)scancode, (Input.Keycode)key, (Input.KeyModifier)modifiers, isPressed, repeat, unicode);
-    }
-
-    private void Window_KeyPressed(Window sender, TimeSpan timestamp, SDL2.NET.Scancode scancode, SDL2.NET.Keycode key, SDL2.NET.KeyModifier modifiers, bool isPressed, bool repeat, uint unicode)
-    {
-        ReportKeyPressed(0, (Input.Scancode)scancode, (Input.Keycode)key, (Input.KeyModifier)modifiers, isPressed, repeat, unicode);
-    }
-
-    private void Window_Hidden(Window sender, TimeSpan timestamp)
-    {
-        IsWindowAvailable = false;
-    }
-
-    private void Window_FocusLost(Window sender, TimeSpan timestamp)
-    {
-        HasFocus = false;
-    }
-
-    private void Window_FocusGained(Window sender, TimeSpan timestamp)
-    {
-        HasFocus = true;
-    }
-
-    private void Window_Shown(Window sender, TimeSpan timestamp)
-    {
-        IsWindowAvailable = true;
-    }
-
-    private void Window_Closed(Window sender, TimeSpan timestamp)
-    {
-        IsWindowAvailable = false;
-    }
-
-    private void Window_SizeChanged(Window window, TimeSpan timestamp, Size newSize)
-    {
-        FrameLock.Wait();
-        try
-        {
-            var (ww, wh) = newSize;
-            Log?.Information("Window size changed to {{w:{width}, h:{height}}}", newSize.Width, newSize.Height);
-
-            //ImGuiController.WindowResized(ww, wh);
-
-            WindowSize = new IntVector2(newSize.Width, newSize.Height);
-        }
-        finally
-        {
-            FrameLock.Release();
-        }
-
-        WindowSizeChanged(timestamp, newSize);
-    }
-
-    /// <inheritdoc/>
-    public override bool TryGetTargetFrameRate([MaybeNullWhen(false), NotNullWhen(true)] out TimeSpan targetFrameRate)
-    {
-        int rate;
-        if (window is null || (rate = Window.DisplayMode.Value.RefreshRate) <= 0)
-        {
-            targetFrameRate = default;
-            return false;
-        }
-
-        targetFrameRate = TimeSpan.FromSeconds(1d / rate);
-        return true;
-    }
-
-    #endregion
 
     #endregion
 
---
+++ VDStudios.MagicEngine.SDL.Base/SDLGraphicsManagerBase.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using SDL2.NET;
+using VDStudios.MagicEngine.Graphics;
+
+namespace VDStudios.MagicEngine.SDL.Base;
+
+/// <summary>
+/// A GraphicsManager base for SDL
+/// </summary>
+public abstract class SDLGraphicsManagerBase<TGraphicsContext> : GraphicsManager<TGraphicsContext>
+    where TGraphicsContext : GraphicsContext<TGraphicsContext>
+{
+    /// <inheritdoc/>
+    protected SDLGraphicsManagerBase(Game game) : base(game) { }
+
+    /// <summary>
+    /// The SDL Window managed by this <see cref="SDLGraphicsManagerBase{TGraphicsContext}"/>
+    /// </summary>
+    public Window Window 
+    { 
+        get => window ?? throw new InvalidOperationException("Cannot get the Window of an SDLGraphicsManager that has not been launched"); 
+        protected set => window = value;
+    }
+    private Window? window;
+
+    /// <summary>
+    /// <see langword="true"/> if <see cref="Window"/> is <see langword="null"/>, <see langword="false"/> otherwise.
+    /// </summary>
+    protected bool IsWindowNull => window is null;
+
+    internal readonly SemaphoreSlim WindowThreadLock = new(1, 1);
+
+    /// <summary>
+    /// The read-only queue containing actions queued through <see cref="PerformOnWindow(Action{Window})"/>
+    /// </summary>
+    protected readonly ConcurrentQueue<Action<Window>> WindowActionQueue = new();
+
+    /// <summary>
+    /// Performs the desired action on <see cref="Window"/>, and returns immediately
+    /// </summary>
+    /// <remarks>
+    /// Failure to perform on <see cref="Window"/> from this method will result in an exception, as <see cref="SDL2.NET.Window"/> is NOT thread-safe and, in fact, thread-protected
+    /// </remarks>
+    /// <param name="action">The action to perform on <see cref="Window"/></param>
+    public void PerformOnWindow(Action<Window> action)
+    {
+        WindowActionQueue.Enqueue(action);
+    }
+
+    /// <inheritdoc/>
+    public override bool TryGetTargetFrameRate([MaybeNullWhen(false), NotNullWhen(true)] out TimeSpan targetFrameRate)
+    {
+        int rate;
+        if (IsWindowNull || (rate = Window.DisplayMode.Value.RefreshRate) <= 0)
+        {
+            targetFrameRate = default;
+            return false;
+        }
+
+        targetFrameRate = TimeSpan.FromSeconds(1d / rate);
+        return true;
+    }
+
+    /// <summary>
+    /// Detaches event listeners from <see cref="Window"/> and disposes of it, setting it to <see langword="null"/> and setting <see cref="GraphicsManager{TGraphicsContext}.IsWindowAvailable"/> to <see langword="false"/>
+    /// </summary>
+    protected void ReleaseWindow()
+    {
+        if (window is Window win) 
+        {
+            win.Closed -= Window_Closed;
+            win.SizeChanged -= Window_SizeChanged;
+            win.Shown -= Window_Shown;
+            win.FocusGained -= Window_FocusGained;
+            win.FocusTaken -= Window_FocusGained;
+            win.FocusLost -= Window_FocusLost;
+            win.Hidden -= Window_Hidden;
+            win.KeyPressed -= Window_KeyPressed;
+            win.KeyReleased -= Window_KeyReleased;
+            win.MouseButtonPressed -= Window_MouseButtonPressed;
+            win.MouseButtonReleased -= Window_MouseButtonReleased;
+            win.MouseMoved -= Window_MouseMoved;
+            win.Dispose();
+        }
+
+        Window = null!;
+        IsWindowAvailable = false;
+    }
+
+    /// <summary>
+    /// Attaches event listeners to <see cref="Window"/> and queries initial data from it
+    /// </summary>
+    protected void ConfigureWindow()
+    {
+        Window.Closed += Window_Closed;
+        Window.SizeChanged += Window_SizeChanged;
+        Window.Shown += Window_Shown;
+        Window.FocusGained += Window_FocusGained;
+        Window.FocusTaken += Window_FocusGained;
+        Window.FocusLost += Window_FocusLost;
+        Window.Hidden += Window_Hidden;
+        Window.KeyPressed += Window_KeyPressed;
+        Window.KeyReleased += Window_KeyReleased;
+        Window.MouseButtonPressed += Window_MouseButtonPressed;
+        Window.MouseButtonReleased += Window_MouseButtonReleased;
+        Window.MouseMoved += Window_MouseMoved;
+        Window.MouseWheelScrolled += Window_MouseWheelScrolled;
+
+        PerformOnWindow(w =>
+        {
+            Log?.Debug("Querying WindowFlags");
+            var flags = w.Flags;
+            IsWindowAvailable = flags.HasFlag(WindowFlags.Shown);
+            HasFocus = flags.HasFlag(WindowFlags.InputFocus);
+
+            Log?.Debug("Reading WindowSize");
+            var (ww, wh) = w.Size;
+            WindowSize = new IntVector2(ww, wh);
+        });
+    }
+
+    #region Window Events
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+    protected void Window_MouseWheelScrolled(Window sender, TimeSpan timestamp, uint mouseId, float verticalScroll, float horizontalScroll)
+    {
+        ReportMouseWheelEvent(mouseId, new Vector2(horizontalScroll, verticalScroll));
+    }
+
+    protected void Window_MouseMoved(Window sender, TimeSpan timestamp, Point delta, Point newPosition, uint mouseId, SDL2.NET.MouseButton pressed)
+    {
+        ReportMouseMoved(mouseId, delta.ToVector2(), newPosition.ToVector2(), (Input.MouseButton)pressed);
+    }
+
+    protected void Window_MouseButtonReleased(Window sender, TimeSpan timestamp, uint mouseId, int clicks, SDL2.NET.MouseButton state)
+    {
+        ReportMouseButtonReleased(mouseId, clicks, (Input.MouseButton)state);
+    }
+
+    protected void Window_MouseButtonPressed(Window sender, TimeSpan timestamp, uint mouseId, int clicks, SDL2.NET.MouseButton state)
+    {
+        ReportMouseButtonPressed(mouseId, clicks, (Input.MouseButton)state);
+    }
+
+    protected void Window_KeyReleased(Window sender, TimeSpan timestamp, SDL2.NET.Scancode scancode, SDL2.NET.Keycode key, SDL2.NET.KeyModifier modifiers, bool isPressed, bool repeat, uint unicode)
+    {
+        ReportKeyReleased(0, (Input.Scancode)scancode, (Input.Keycode)key, (Input.KeyModifier)modifiers, isPressed, repeat, unicode);
+    }
+
+    protected void Window_KeyPressed(Window sender, TimeSpan timestamp, SDL2.NET.Scancode scancode, SDL2.NET.Keycode key, SDL2.NET.KeyModifier modifiers, bool isPressed, bool repeat, uint unicode)
+    {
+        ReportKeyPressed(0, (Input.Scancode)scancode, (Input.Keycode)key, (Input.KeyModifier)modifiers, isPressed, repeat, unicode);
+    }
+
+    protected void Window_Hidden(Window sender, TimeSpan timestamp)
+    {
+        IsWindowAvailable = false;
+    }
+
+    protected void Window_FocusLost(Window sender, TimeSpan timestamp)
+    {
+        HasFocus = false;
+    }
+
+    protected void Window_FocusGained(Window sender, TimeSpan timestamp)
+    {
+        HasFocus = true;
+    }
+
+    protected void Window_Shown(Window sender, TimeSpan timestamp)
+    {
+        IsWindowAvailable = true;
+    }
+
+    protected void Window_Closed(Window sender, TimeSpan timestamp)
+    {
+        IsWindowAvailable = false;
+    }
+
+    protected void Window_SizeChanged(Window window, TimeSpan timestamp, Size newSize)
+    {
+        IntVector2 oSize;
+        IntVector2 nSize = new(newSize.Width, newSize.Height);
+        FrameLock.Wait();
+        try
+        {
+            var (ww, wh) = newSize;
+            Log?.Information("Window size changed to {{w:{width}, h:{height}}}", newSize.Width, newSize.Height);
+
+            //ImGuiController.WindowResized(ww, wh);
+
+            oSize = WindowSize;
+            WindowSize = nSize;
+        }
+        finally
+        {
+            FrameLock.Release();
+        }
+
+        WindowSizeChanged(oSize, nSize);
+    }
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+    #endregion
+}
