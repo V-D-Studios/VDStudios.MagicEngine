a772af47288d028a7884893bd4b80ae6d8f57e43
Diego Garcia <dagarciam1014@gmail.com>
2023-09-13 02:49:01 -0400

    Create TexturedShape2DRenderer.cs

---
+++ VDStudios.MagicEngine.Graphics.Veldrid/DrawOperations/TexturedShape2DRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+using VDStudios.MagicEngine.Geometry;
+using VDStudios.MagicEngine.Graphics.Veldrid.GPUTypes;
+using VDStudios.MagicEngine.Graphics.Veldrid.GPUTypes.Interfaces;
+using Veldrid;
+
+namespace VDStudios.MagicEngine.Graphics.Veldrid.DrawOperations;
+
+/// <summary>
+/// An operation that renders a texture on top of a <see cref="ShapeDefinition2D"/>
+/// </summary>
+public class TexturedShape2DRenderer<TVertex> : Shape2DRenderer<TVertex>
+    where TVertex : unmanaged, IVertexType<TVertex>
+{
+    /// <summary>
+    /// Creates a new object of type <see cref="Shape2DRenderer{TVertex}"/>
+    /// </summary>
+    public TexturedShape2DRenderer(
+        ShapeDefinition2D shape,
+        Game game,
+        IVertexGenerator<Vector2, TVertex>? vertexGenerator,
+        ElementSkip vertexSkip = default
+    )
+        : base(shape, game, vertexGenerator, vertexSkip)
+    {
+    }
+
+    private Func<IVeldridGraphicsContextResources, Texture> textureFactory;
+    private Func<IVeldridGraphicsContextResources, Sampler> samplerFactory;
+    private Func<IVeldridGraphicsContextResources, TextureView> viewFactory;
+
+    protected override void CreateGPUResources(VeldridGraphicsContext context)
+    {
+#warning Create the pipeline, don't call `base`
+// Textures are cached, capitalize on that
+// Samplers are cached, capitalize on that
+    }
+}
