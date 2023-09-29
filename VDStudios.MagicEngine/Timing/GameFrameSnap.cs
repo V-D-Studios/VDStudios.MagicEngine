7b83e759768b2948ba297760e6275b255515b75f
Diego Garcia <dagarciam1014@gmail.com>
2023-09-29 01:27:27 -0400

    Introduced FrameSnaps

---
+++ VDStudios.MagicEngine/Timing/GameFrameSnap.cs
@@ -0,0 +1,15 @@
+﻿namespace VDStudios.MagicEngine.Timing;
+
+/// <summary>
+/// Represents a snap of a given frame of <paramref name="Game"/>, to later present the difference between the current frame and this one
+/// </summary>
+/// <param name="Game"></param>
+public readonly record struct GameFrameSnap(Game Game)
+{
+    private readonly ulong Start = Game.FrameCount;
+
+    /// <summary>
+    /// The current offset between <see cref="Game"/>'s current frame and the one snapped by this object
+    /// </summary>
+    public uint Offset => (uint)(Game.FrameCount - Start);
+}
---
+++ VDStudios.MagicEngine/Timing/GraphicsManagerFrameSnap.cs
@@ -0,0 +1,17 @@
+﻿using VDStudios.MagicEngine.Graphics;
+
+namespace VDStudios.MagicEngine.Timing;
+
+/// <summary>
+/// Represents a snap of a given frame of <paramref name="GraphicsManager"/>, to later present the difference between the current frame and this one
+/// </summary>
+/// <param name="GraphicsManager"></param>
+public readonly record struct GraphicsManagerFrameSnap(GraphicsManager GraphicsManager)
+{
+    private readonly ulong Start = GraphicsManager.FrameCount;
+
+    /// <summary>
+    /// The current offset between <see cref="GraphicsManager"/>'s current frame and the one snapped by this object
+    /// </summary>
+    public uint Offset => (uint)(GraphicsManager.FrameCount - Start);
+}
