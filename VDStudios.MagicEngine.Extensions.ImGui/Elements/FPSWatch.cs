fe6a9da91a3f6d8ab95c42cf7553198fdbc71c35
Diego Garcia <dagarciam1014@gmail.com>
2023-10-01 17:21:39 -0400

    Create FPSWatch.cs

---
+++ VDStudios.MagicEngine.Extensions.ImGui/Elements/FPSWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+using VDStudios.MagicEngine.Graphics;
+using VDStudios.MagicEngine.Timing;
+
+namespace VDStudios.MagicEngine.Extensions.ImGuiExtension.Elements;
+
+/// <summary>
+/// An ImGui element that keeps track of the FPS of the <see cref="GraphicsManager"/> it belongs to
+/// </summary>
+public class FPSWatch : ImGUIElement
+{
+    /// <inheritdoc/>
+    public FPSWatch(Game game) : base(game) { }
+
+    private string fpsstring = "Not Measured";
+    private GraphicsManagerFrameTimer timer;
+
+    /// <inheritdoc/>
+    protected internal override void SubmitUI(TimeSpan delta, GraphicsManager graphicsManager)
+    {
+        if (timer.IsDefault)
+            timer = new GraphicsManagerFrameTimer(graphicsManager, 10);
+
+        if (timer.HasClocked)
+        {
+            fpsstring = graphicsManager.FramesPerSecond.ToString("0.00");
+            timer.Restart();
+        }
+
+        ImGui.Begin("FPS Watch");
+        ImGui.Text(fpsstring);
+        ImGui.End();
+    }
+}
